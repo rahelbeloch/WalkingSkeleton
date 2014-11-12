a162a9d2e69696c436d071237e8991a54df8df50
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-12 16:58:30 +0000

    Initial Commit

---
+++ backend/javaserver/src/main/java/de/hsrm/mi/gruppe02/javaserver/App.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.gruppe02.javaserver;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
