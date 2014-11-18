8244cde46d1815145d8ea6c49e8e87afa2e17f67
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-18 13:31:45 +0000

    #23 - implemented dummy data structure (model) for testing purposes.

---
+++ frontend/AdminClient/model/DummyWorkflow.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminClient.model
+{
+    class DummyWorkflow
+    {
+        private IList<DummyStep> _steps;
+
+        public DummyWorkflow()
+        {
+            _steps = new List<DummyStep>();
+
+            // init with default values for testing purposes
+            AddStep(new DummyStartStep("michel"));
+            AddStep(new DummyAction("aktion 1", "klaus"));
+            AddStep(new DummyAction("aktion zwei", "michel"));
+            AddStep(new DummyFinalStep());
+        }
+
+        public void AddStep(DummyStep step)
+        {
+            _steps.Add(step);
+        }
+
+        public void RemoveLastStep()
+        {
+            _steps.RemoveAt(_steps.Count - 1);
+        }
+    }
+}
