e507beb3c99b3b1cc63a169d5a8c0ea2b43ecb32
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-18 13:31:45 +0000

    #23 - implemented dummy data structure (model) for testing purposes.

---
+++ frontend/AdminClient/model/DummyStartStep.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminClient.model
+{
+    class DummyStartStep : DummyStep
+    {
+        public DummyStartStep(string user)
+        {
+            type = "Start";
+            this.user = user;
+        }
+    }
+}
