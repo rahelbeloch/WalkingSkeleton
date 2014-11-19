afb1a9fbee359b314eeb6d15dcbe4304fd197556
khils001 <khils001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 13:44:40 +0000

    #29 init for login and dashbord

---
+++ frontend/UserClient/ViewModel/ViewModelBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserClient.util
+{
+    /// <summary>
+    /// Default base class for ViewModels in the MVVM pattern
+    /// with boilerplate-clode for property changes.
+    /// </summary>
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnChanged(string propertyName)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
