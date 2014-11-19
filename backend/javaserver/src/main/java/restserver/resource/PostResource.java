6c9fa61ee4ed16fb0ca11cbd2463ea3691952198
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 14:26:49 +0000

    #21 added comments

--- backend/javaserver/src/main/java/restserver/resource/PostResource.java
+++ backend/javaserver/src/main/java/restserver/resource/PostResource.java
@@ -9,11 +9,22 @@ import javax.ws.rs.core.Response;
 import persistence.Persistence;
 import beans.Workflow;
 
+/**
+ * 
+ * @author akoen001
+ *
+ * REST Service, storing given information in the database
+ */
 @Path("send")
 public class PostResource {
 	
 	Persistence db = new Persistence();
 	
+	/**
+	 * 
+	 * @param receivedWorkflow
+	 * @return Response if storage was successful
+	 */
 	@POST @Path("workflow")
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response saveWorkflow (Workflow receivedWorkflow) {
