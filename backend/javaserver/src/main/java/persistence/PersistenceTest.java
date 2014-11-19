937d3ded7708e9ec2ad03e23e3e2d7a8782ab71b
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 09:34:07 +0000

    #20 -- deleted class PersistenceTEST

---
+++ backend/javaserver/src/main/java/persistence/PersistenceTest.java
@@ -0,0 +1,14 @@
+package persistence;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class PersistenceTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
