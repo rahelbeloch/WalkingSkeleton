8aa625ad4ea95d3554c8c75522f3e9cd6b000bfe
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 10:03:00 +0000

    #31 corrected comments in processmanager and method splitting in startprocessor

--- backend/javaserver/src/main/java/processor/StartProcessor.java
+++ backend/javaserver/src/main/java/processor/StartProcessor.java
@@ -21,14 +21,23 @@ public class StartProcessor {
 	}
 	
 	/**
-	 * This method receives an workflow-object and creates an item according to the workflow's initialized steps.
-	 * The item consists of pairs of step IDs and its MetaState. The default MetaState is INACTIVE. 
-	 * Only "Action" step-objects are considered. The state of the very first step is set to "OPEN".
+	 * This method receives an workflow-object and creates an item and calls a method which instantiates its Metadata
+	 * according to the workflow's initialized steps. The item consists of pairs of step IDs and its MetaState. 
 	 * @param workflow
 	 */
 	public void createItem(Workflow workflow){
 		currentItem = new Item();
-		
+		//irgendetwas passiert hier noch
+		initiateItem(workflow, currentItem);
+	}
+	
+	/**
+	 * This method instantiates an item's Metadata. The default MetaState is INACTIVE. 
+	 * Only "Action" step-objects are considered. The state of the very first step is set to "OPEN".
+	 * @param workflow
+	 * @param item
+	 */
+	public void initiateItem(Workflow workflow, Item item){
 		for (Step s: workflow.getStep()){
 			if (s instanceof Action){ 
 				currentItem.getMetadata().getKey().add(s.getId());
