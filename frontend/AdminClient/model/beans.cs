4371588895e687ab233cf13f6e94289cd090d5e4
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 13:29:35 +0000

    #19 - update beans with startstep

--- frontend/AdminClient/model/beans.cs
+++ frontend/AdminClient/model/beans.cs
@@ -17,6 +17,7 @@ using System.Xml.Serialization;
 
 /// <remarks/>
 [System.Xml.Serialization.XmlIncludeAttribute(typeof(Action))]
+[System.Xml.Serialization.XmlIncludeAttribute(typeof(StartStep))]
 [System.Xml.Serialization.XmlIncludeAttribute(typeof(FinalStep))]
 [System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
 [System.SerializableAttribute()]
@@ -48,6 +49,27 @@ public partial class Step {
         }
         set {
             this.nextStepsField = value;
+        }
+    }
+}
+
+/// <remarks/>
+[System.CodeDom.Compiler.GeneratedCodeAttribute("xsd", "2.0.50727.3038")]
+[System.SerializableAttribute()]
+[System.Diagnostics.DebuggerStepThroughAttribute()]
+[System.ComponentModel.DesignerCategoryAttribute("code")]
+[System.Xml.Serialization.XmlTypeAttribute(Namespace="http://www.example.org/Beans")]
+public partial class StartStep : Step {
+    
+    private int userIdField;
+    
+    /// <remarks/>
+    public int UserId {
+        get {
+            return this.userIdField;
+        }
+        set {
+            this.userIdField = value;
         }
     }
 }
