ddfb645508b119e732f39c9cd1be11494489a1b8
khils001 <khils001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-19 13:44:40 +0000

    #29 init for login and dashbord

---
+++ frontend/UserClient/ViewModel/MainViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserClient.ViewModel
+{
+    class MainViewModel
+    {
+        public MainViewModel()
+        {
+            
+        }
+    }
+}
