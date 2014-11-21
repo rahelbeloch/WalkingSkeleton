ff2517021f4620a4d3eb7234f398a2e3b355841e
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-21 11:41:17 +0000

    #19 #24 - added correct beans in 'model' folder

---
+++ frontend/RestAPI/model/backingbeans/FinalStep.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdminClient.model
+{
+    /// <summary>
+    /// Backing bean implementation for FinalStep.
+    /// </summary>
+    class FinalStep : AbstractFinalStep
+    {
+    }
+}
