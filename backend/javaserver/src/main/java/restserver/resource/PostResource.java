383983dadb560218aba3cdb0ec4ed7f1f34c6118
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-24 15:16:50 +0000

    #21 Added forward and start services(incomplete)

--- backend/javaserver/src/main/java/restserver/resource/PostResource.java
+++ backend/javaserver/src/main/java/restserver/resource/PostResource.java
@@ -3,11 +3,12 @@ package restserver.resource;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import abstractbeans.AbstractWorkflow;
-import persistence.Persistence;
+import persistence.PersistenceImp;
 
 /**
  * 
@@ -15,20 +16,32 @@ import persistence.Persistence;
  *
  * REST Service, storing given information in the database
  */
-@Path("send")
 public class PostResource {
 	
-	Persistence db = new Persistence();
+	PersistenceImp db = new PersistenceImp();
 	
 	/**
 	 * 
 	 * @param receivedWorkflow
 	 * @return Response if storage was successful
 	 */
-	@POST @Path("workflow")
+	@POST @Path("send/workflow")
 	@Consumes(MediaType.APPLICATION_XML)
 	public Response saveWorkflow (AbstractWorkflow receivedWorkflow) {
 		db.storeWorkflow(receivedWorkflow);
+		return Response.status(201).build();
+	}
+	
+	@POST @Path("start/{workflowid}/{userid}")
+	public Response startWorkflow (@PathParam("workflowid") int workflowid, @PathParam("userid") int userid) {
+		//Workflow starten
+		return Response.status(201).build();
+	}
+	
+	@POST @Path("forward/{stepid}/{itemid}/{userid}")
+	public Response forward (@PathParam("stepid") int stepid, @PathParam("itemid") int itemid,
+							 @PathParam("userid") int userid) {
+		//Weiterschalten
 		return Response.status(201).build();
 	}
 	
