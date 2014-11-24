c3f30c1a6bd74545c2aa8098e4f57e7f48d9faf4
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-24 13:07:27 +0000

    #34 IDataReceiver implemented

---
+++ frontend/RestAPI/IDataReceiver.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace CommunicationLib
+{
+    /// <summary>
+    /// Interface for client callback methods
+    /// </summary>
+    class IDataReceiver
+    {
+        public void WorkflowUpdate(RegistrationWrapper<AbstractWorkflow> wrappedObject);
+        public void ItemUpdate(RegistrationWrapper<AbstractItem> wrappedObject);
+        public void UserUpdate(RegistrationWrapper<AbstractUser> wrappedObject);
+    }
+}
