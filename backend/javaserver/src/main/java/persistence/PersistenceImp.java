174c12bb5a23678b33d95e6af03afab327d5ea52
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-24 10:40:53 +0000

    #20 -- filled load, store and delete function for all dataObejcts on persistence layer

--- backend/javaserver/src/main/java/persistence/PersistenceImp.java
+++ backend/javaserver/src/main/java/persistence/PersistenceImp.java
@@ -4,56 +4,60 @@ import java.util.LinkedList;
 import java.util.List;
 
 import abstractbeans.AbstractItem;
+import abstractbeans.AbstractMetaEntry;
 import abstractbeans.AbstractStep;
 import abstractbeans.AbstractUser;
 import abstractbeans.AbstractWorkflow;
 
-public class PersistenceImp {
-	
+public class PersistenceImp implements Persistence {
+
 	/*
-	 * Abstraktion einer Datenbank, die Objekte in der Form Abstract-* persistiert
+	 * abstraction of a database, that persists object in form Abstract-*
 	 */
 	private List<AbstractWorkflow> workflows = new LinkedList<>();
-	private List<AbstractUser> users = new LinkedList<>();
 	private List<AbstractItem> items = new LinkedList<>();
-	private List<AbstractStep> steps = new LinkedList<>();
-
-//	public AbstractItem resolveMap(Item item) {
-//		AbstractItem tempItem = null;
-//		AbstractMap tempMap = null;
-//		
-//		/*
-//		 * following code does not work because structure will be redefined by Jerome/Jane
-//		 * tempMap.getKey().addAll(item.getMetaData().getKeys());
-//		 * tempMap.getValue().addAll(item.getMetaData().getValues());
-//		 */
-//		tempItem.setMetadata(tempMap);
-//		return tempItem;
-//	}
+	private List<AbstractUser> users = new LinkedList<>();
 	
-
+	private List<AbstractStep> steps = new LinkedList<>();
+	private List<AbstractMetaEntry> metaEntries = new LinkedList<>();
+	
 	/*
-	 * store functions for steps, items, users, workflows
+	 * store functions for workflow, item, user
+	 * store functions for step, metaEntry
 	 */	
-	public void storeStep(AbstractStep step) {
-		for(AbstractStep s: steps) {
-			if(s.getId() == step.getId()) {
-				steps.remove(s);
+	@Override
+	public void storeWorkflow(AbstractWorkflow workflow) {
+		for(AbstractWorkflow wf: workflows) {
+			if (wf.getId() == workflow.getId()) {
+				workflows.remove(wf);
 			}
 		}
-		steps.add((AbstractStep)step);
+		workflows.add((AbstractWorkflow)workflow);
+		
+		// a workflows steps are resolved and stored one by one
+		List<AbstractStep> workflowsSteps = workflow.getStep();
+		for(AbstractStep step: workflowsSteps) {
+			storeStep(step);
+		}
 	}
-	
+
+	@Override
 	public void storeItem(AbstractItem item) {
 		for(AbstractItem i: items) {
 			if(i.getId() == item.getId()) {
 				items.remove(i);
 			}
 		}
-		// items includes a HashMap that has to be transfered into lists (Map)
 		items.add((AbstractItem)item);
+		
+		// items include type MetaEntry that has to be stored separately
+		List<AbstractMetaEntry> itemsMetadata = item.getMetadata();
+		for(AbstractMetaEntry metaEntry: itemsMetadata) {
+			storeMetaEntry(metaEntry);
+		}
 	}
-	
+
+	@Override
 	public void storeUser(AbstractUser user) {
 		for(AbstractUser u: users) {
 			if(u.getId() == user.getId()) {
@@ -63,29 +67,43 @@ public class PersistenceImp {
 		users.add((AbstractUser)user);
 	}
 	
-	public void storeWorkflow(AbstractWorkflow workflow) {
-		for(AbstractWorkflow wf: workflows) {
-			if (wf.getId() == workflow.getId()) {
-				workflows.remove(wf);
+	public void storeStep(AbstractStep step) {
+		for(AbstractStep s: steps) {
+			if(s.getId() == step.getId()) {
+				steps.remove(s);
 			}
 		}
-		workflows.add((AbstractWorkflow)workflow);
+		steps.add((AbstractStep)step);
+		// need to distinguish between Action/FirstStep/StartStep?
+	}
+	
+	public void storeMetaEntry(AbstractMetaEntry metaEntry) {
+		for(AbstractMetaEntry me: metaEntries) {
+			// assumption that MetaEntries have keys that are unique
+			if(me.getKey() == metaEntry.getKey()) {
+				metaEntries.remove(me);
+			}
+		}
+		metaEntries.add((AbstractMetaEntry)metaEntry);
+	}
+	
+	
+	/*
+	 * load functions for workflow, item, user
+	 * load functions for step, metaEntry
+	 */
+	@Override
+	public AbstractWorkflow loadWorkflow(int id) {
+		AbstractWorkflow workflow = null;
+		for(AbstractWorkflow wf: workflows){
+			if(wf.getId() == id) {
+				workflow = wf;
+			}
+		}
+		return workflow;
 	}
 
-	/*
-	 * load functions for steps, items, users, workflows
-	 */
-	
-	public AbstractStep loadStep(int id) {
-		AbstractStep step = null;
-		for(AbstractStep s: steps) {
-			if(s.getId() == id) {
-				step = s;
-			}
-		}
-		return step;
-	}
-	
+	@Override
 	public AbstractItem loadItem(int id) {
 		AbstractItem item = null;
 		for(AbstractItem i: items) {
@@ -95,7 +113,8 @@ public class PersistenceImp {
 		}
 		return item;
 	}
-	
+
+	@Override
 	public AbstractUser loadUser(int id) {
 		AbstractUser user = null;
 		for(AbstractUser u: users) {
@@ -106,27 +125,21 @@ public class PersistenceImp {
 		return user;
 	}
 	
-	public AbstractWorkflow loadWorkflow(int id) {
-		AbstractWorkflow workflow = null;
-		for(AbstractWorkflow wf: workflows){
-			if(wf.getId() == id) {
-				workflow = wf;
-			}
-		}
-		return workflow;
-	}
 	
 	/*
-	 * delete functions for steps, items, users, workflows
-	 */
-	public void deleteStep(int id) {
-		for(AbstractStep s: steps) {
-			if(s.getId() == id) {
-				steps.remove(s);
+	 * delete functions for workflow, item, user
+	 * delete functions for step, metaEntry
+	 */	
+	@Override
+	public void deleteWorkflow(int id) {
+		for(AbstractWorkflow wf: workflows) {
+			if(wf.getId() == id) {
+				workflows.remove(wf);
 			}
 		}
 	}
 	
+	@Override
 	public void deleteItem(int id) {
 		for(AbstractItem i: items) {
 			if(i.getId() == id) {
@@ -135,6 +148,7 @@ public class PersistenceImp {
 		}
 	}
 	
+	@Override
 	public void deleteUser(int id) {
 		for(AbstractUser u: users) {
 			if(u.getId() == id) {
@@ -143,12 +157,19 @@ public class PersistenceImp {
 		}
 	}
 	
-	public void deleteWorkflow(int id) {
-		for(AbstractWorkflow wf: workflows) {
-			if(wf.getId() == id) {
-				workflows.remove(wf);
+	public void deleteStep(int id) {
+		for(AbstractStep s: steps) {
+			if(s.getId() == id) {
+				steps.remove(s);
 			}
 		}
 	}
-
+	
+	public void deleteMetaEntry(String key) {
+		for(AbstractMetaEntry me: metaEntries) {
+			if(me.getKey() == key) {
+				metaEntries.remove(me);
+			}
+		}
+	}
 }
