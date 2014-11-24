74d2f9c98a2a0a44b2a17e2dcc386b82f41e39c1
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-24 10:40:53 +0000

    #20 -- filled load, store and delete function for all dataObejcts on persistence layer

--- backend/javaserver/src/main/java/persistence/Persistence.java
+++ backend/javaserver/src/main/java/persistence/Persistence.java
@@ -1,5 +1,30 @@
 package persistence;
 
+import abstractbeans.AbstractItem;
+import abstractbeans.AbstractUser;
+import abstractbeans.AbstractWorkflow;
+
 public interface Persistence {
+	
+	/*
+	 * store functions to store workflows, items, and users into persistence
+	 */
+	public void storeWorkflow(AbstractWorkflow workflow);
+	public void storeItem(AbstractItem item);
+	public void storeUser(AbstractUser user);
+	
+	/*
+	 * load functions to get workflows, items, and users from persistence
+	 */
+	public AbstractWorkflow loadWorkflow(int id);
+	public AbstractItem loadItem(int id);
+	public AbstractUser loadUser(int id);
+
+	/*
+	 * delete functions to remove workflows, items, and users from persistence
+	 */
+	public void deleteWorkflow(int id);
+	public void deleteItem(int id);
+	public void deleteUser(int id);
 
 }
