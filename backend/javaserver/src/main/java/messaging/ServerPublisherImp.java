f78c27c79a0e9d74dd74af0265058e108767c59c
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-25 09:07:24 +0000

    #34 added commentaries (again)

--- backend/javaserver/src/main/java/messaging/ServerPublisherImp.java
+++ backend/javaserver/src/main/java/messaging/ServerPublisherImp.java
@@ -12,11 +12,9 @@ import org.apache.activemq.broker.BrokerService;
 
 import com.google.inject.Singleton;
 
-/** //TODO good Class description
- *  Class for Messaging on server-side.
- *  Uses an embeded message broker.
- * 
- * @author sehre001
+/** Class for message publishing on server-side.
+ *  Uses ActiveMQ as embedded message broker.
+ *  Enables string publishing on a specified message topic.
  */
 @Singleton
 public class ServerPublisherImp implements ServerPublisher {
@@ -26,8 +24,9 @@ public class ServerPublisherImp implements ServerPublisher {
 	private MessageProducer publisher;
 	private BrokerService broker;
 	
-	//broker will run in the same VM (embeded)
+	//broker will run in the same VM (embedded)
 	final private String BROKER_URL = "vm://localhost";
+	final private String CONNECTION_URL = "tcp://0.0.0.0:61616";
 	
 	public ServerPublisherImp() {
 		
@@ -48,7 +47,7 @@ public class ServerPublisherImp implements ServerPublisher {
 		publisher = session.createProducer(topic);
 		publisher.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
 		
-		//define messaging-content (TextMessage/MapMessage)
+		//define messaging-content (TextMessage or MapMessage)
 		TextMessage msg = session.createTextMessage(content);
 		
 		//send and close
@@ -59,27 +58,32 @@ public class ServerPublisherImp implements ServerPublisher {
 	}
 	
 	/**
-	 * Starts the embeded messaging broker
+	 * Starts the embedded messaging broker
 	 */
 	public void startBroker() {
 		broker = new BrokerService();
-		try {
-			broker.addConnector("tcp://0.0.0.0:61616");
-			broker.start();
-			System.out.println("Broker started...");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			//Error: No Broker available
-		}	
+		if (!broker.isStarted()) {
+			try {
+				broker.addConnector(CONNECTION_URL);
+				broker.start();
+				System.out.println("Message-Broker started...");
+			} catch (Exception ex) {
+				ex.printStackTrace();
+			}
+		}
+		else {
+			System.err.println("Broker allready running!");
+		}
+
 	}
 	
 	/**
-	 * Stops the embeded messaging broker
+	 * Stops the embedded messaging broker
 	 */
 	public void stopBroker(){
 		try {
 			broker.stop();
-			System.out.println("Broker stopped...");
+			System.out.println("Message-Broker stopped...");
 		} catch (Exception ex) {
 			ex.printStackTrace();
 		}
