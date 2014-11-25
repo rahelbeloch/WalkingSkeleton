135613670b9d7369c3a9d869aa2ebd581af34695
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-25 13:15:20 +0000

    #23 - added possibility to add a step to the workflow with correct link from previous step

--- frontend/RestAPI/model/backingbeans/Workflow.cs
+++ frontend/RestAPI/model/backingbeans/Workflow.cs
@@ -11,5 +11,21 @@ namespace CommunicationLib.Model
     /// </summary>
     public partial class Workflow
     {
+        /// <summary>
+        /// Add new step to workflow. The new step is also added to the "nextSteps" list of the previous step.
+        /// </summary>
+        /// <param name="step"></param>
+        public void addStep(Step step)
+        {
+            if (Step.Count == 0)
+            {
+                Step.Add(step);
+            }
+            else if (Step.Count > 0)
+            {
+                Step[Step.Count - 1].nextSteps.Add(step);
+                Step.Add(step);
+            }
+        }
     }
 }
