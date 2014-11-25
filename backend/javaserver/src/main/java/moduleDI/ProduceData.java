7321a67f5e830a0bdc0618fcd0719ec77d8e9860
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-25 16:48:22 +0000

    #33 sample data

---
+++ backend/javaserver/src/main/java/moduleDI/ProduceData.java
@@ -0,0 +1,49 @@
+package moduleDI;
+
+import backingbeans.Action;
+import backingbeans.FinalStep;
+import backingbeans.User;
+import backingbeans.Workflow;
+import persistence.Persistence;
+import persistence.UserAlreadyExistsException;
+
+public class ProduceData {
+	
+	private int countWorkflow = 0;
+	private int countUser = 0;
+	
+	
+	
+	/**
+	 * Create a sample Workflow
+	 * @param p persistence
+	 */
+	public void createWorkflow(Persistence p){
+		
+		Workflow w =new Workflow(countWorkflow);
+		w.addStep(new Action(0, 2, "Eins"));
+		w.addStep(new Action(0, 2, "Zwei"));
+		w.addStep(new Action(0, 2, "Drei"));
+		w.addStep(new FinalStep());
+		
+		p.storeWorkflow(w);
+		
+		countWorkflow++;
+	}
+	/**
+	 * Create a sample User
+	 */
+	public void createUser(Persistence p){
+		User user = new User();
+		user.setId(countUser);
+		user.setName(countUser+"");
+		try {
+			p.addUser(user);
+		} catch (UserAlreadyExistsException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		countUser++;
+	}
+
+}
