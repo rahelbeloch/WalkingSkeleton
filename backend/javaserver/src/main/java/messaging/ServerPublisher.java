a7e7aa49329c32f687e71ec89d897a9290f5a900
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-25 08:57:33 +0000

    #31 added interface for google guice dependency injection, serverpublisherimp is default-implementation annotated as singleton, serverpublisher is interface

--- backend/javaserver/src/main/java/messaging/ServerPublisher.java
+++ backend/javaserver/src/main/java/messaging/ServerPublisher.java
@@ -1,88 +1,18 @@
 package messaging;
 
-import javax.jms.Connection;
-import javax.jms.DeliveryMode;
-import javax.jms.MessageProducer;
-import javax.jms.Session;
-import javax.jms.TextMessage;
-import javax.jms.Topic;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
-import org.apache.activemq.broker.BrokerService;
-
-/** Class for message publishing on server-side.
- *  Uses ActiveMQ as embedded message broker.
- *  String messages can be published under specific topic names.
+/**
+ * Interface for ServerPublisher
+ * @author jvanh001
+ *
  */
-public class ServerPublisher {
+public interface ServerPublisher {
+
 	
-	private Connection connection;
-	private Session session;
-	private MessageProducer publisher;
-	private BrokerService broker;
-	
-	//broker will run in the same VM (embedded)
-	final private String BROKER_URL = "vm://localhost";
-	final private String CONNECTOR_URL = "tcp://0.0.0.0:61616";
-	
-	public ServerPublisher() {
-		
-	}
-	
-	/**Publishes a String-content on a specified topic.
-	 * @param content - content to publish
-	 * @param topicName - topic where the content will be published
-	 * @throws Exception
-	 */
-	public void publish(String content, String topicName) throws Exception {
-		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
-		connection = factory.createConnection();
-		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-		
-		//create a message-producer for the topic
-		Topic topic = session.createTopic(topicName);
-		publisher = session.createProducer(topic);
-		publisher.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-		
-		//define messaging-content (TextMessage or MapMessage)
-		TextMessage msg = session.createTextMessage(content);
-		
-		//send and close
-		System.out.println("INHALT: " + msg.getText());
-		publisher.send(msg);
-		session.close();
-		connection.close();
-	}
-	
-	/**
-	 * Starts the embedded messaging broker
-	 * Uses the CONNECTOR_URL specified by <class>ServerPublisher</class>
-	 */
-	public void startBroker() {
-		broker = new BrokerService();
-		if (!broker.isStarted()){
-			try {
-				broker.addConnector(CONNECTOR_URL);
-				broker.start();
-			} catch (Exception ex) {
-				//Error: No Broker available
-				ex.printStackTrace();
-			}	
-		}
-		else {
-			System.out.println("Broker allready running! (" + CONNECTOR_URL + ")");
-		}
-	}
-	
-	/**
-	 * Stops the embedded messaging broker
-	 */
-	public void stopBroker(){
-		try {
-			broker.stop();
-			System.out.println("Broker stopped...");
-		} catch (Exception ex) {
-			ex.printStackTrace();
-		}
-	}
+	public void publish(String content, String topicName) throws Exception;
+
+	public void startBroker();
+
+	public void stopBroker();
 }
+
---
+++ backend/javaserver/src/main/java/messaging/ServerPublisherImp.java
@@ -0,0 +1,87 @@
+package messaging;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.jms.Topic;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.broker.BrokerService;
+
+import com.google.inject.Singleton;
+
+/** //TODO good Class description
+ *  Class for Messaging on server-side.
+ *  Uses an embeded message broker.
+ * 
+ * @author sehre001
+ */
+@Singleton
+public class ServerPublisherImp implements ServerPublisher {
+	
+	private Connection connection;
+	private Session session;
+	private MessageProducer publisher;
+	private BrokerService broker;
+	
+	//broker will run in the same VM (embeded)
+	final private String BROKER_URL = "vm://localhost";
+	
+	public ServerPublisherImp() {
+		
+	}
+	
+	/**Publishes a String-content on a specified topic.
+	 * @param content - content to publish
+	 * @param topicName - topic where the content will be published
+	 * @throws Exception
+	 */
+	public void publish(String content, String topicName) throws Exception {
+		ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(BROKER_URL);
+		connection = factory.createConnection();
+		session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+		
+		//create a message-producer for the topic
+		Topic topic = session.createTopic(topicName);
+		publisher = session.createProducer(topic);
+		publisher.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+		
+		//define messaging-content (TextMessage/MapMessage)
+		TextMessage msg = session.createTextMessage(content);
+		
+		//send and close
+		System.out.println("INHALT: " + msg.getText());
+		publisher.send(msg);
+		session.close();
+		connection.close();
+	}
+	
+	/**
+	 * Starts the embeded messaging broker
+	 */
+	public void startBroker() {
+		broker = new BrokerService();
+		try {
+			broker.addConnector("tcp://0.0.0.0:61616");
+			broker.start();
+			System.out.println("Broker started...");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			//Error: No Broker available
+		}	
+	}
+	
+	/**
+	 * Stops the embeded messaging broker
+	 */
+	public void stopBroker(){
+		try {
+			broker.stop();
+			System.out.println("Broker stopped...");
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+}
