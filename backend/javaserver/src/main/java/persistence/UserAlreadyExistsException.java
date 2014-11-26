facbfa4d54f8f93cf9060ed6472f0dce395562ab
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 13:45:44 +0000

    #20 improved exception definition for persistence

--- backend/javaserver/src/main/java/persistence/UserAlreadyExistsException.java
+++ backend/javaserver/src/main/java/persistence/UserAlreadyExistsException.java
@@ -1,5 +1,14 @@
 package persistence;
 
 public class UserAlreadyExistsException extends Exception {
+
+	private static final long serialVersionUID = 4328135084952400723L;
 	
+	public UserAlreadyExistsException(){
+		super();
+	}
+	
+	public UserAlreadyExistsException(String msg){
+		super(msg);
+	}
 }
--- backend/javaserver/src/main/java/persistence/UserNotExistentException.java
+++ backend/javaserver/src/main/java/persistence/UserNotExistentException.java
@@ -2,4 +2,13 @@ package persistence;
 
 public class UserNotExistentException extends Exception {
 
+	private static final long serialVersionUID = 3157473446452493460L;
+
+	public UserNotExistentException() {
+		super();
+	}
+	
+	public UserNotExistentException(String msg) {
+		super(msg);
+	}
 }
