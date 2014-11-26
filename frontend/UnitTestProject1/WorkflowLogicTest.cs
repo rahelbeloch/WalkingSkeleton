a87804b1934ed17e413dad982fa46f6811659065
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 16:29:17 +0000

    #19 - fixed CommunicationLibTests project with correct model

--- frontend/UnitTestProject1/WorkflowLogicTest.cs
+++ frontend/UnitTestProject1/WorkflowLogicTest.cs
@@ -16,17 +16,17 @@ namespace UnitTestProject1
         [TestMethod]
         public void addStepTest()
         {
-            AbstractWorkflow wf = new AbstractWorkflow();
+            Workflow wf = new Workflow();
 
-            AbstractStep st1 = new AbstractStep();
-            AbstractStep st2 = new AbstractStep();
-            AbstractStep st3 = new AbstractStep();
+            Step st1 = new Step();
+            Step st2 = new Step();
+            Step st3 = new Step();
 
             wf.addStep(st1);
             wf.addStep(st2);
             wf.addStep(st3);
 
-            Assert.IsTrue(wf.Step[2] == st3);
+            Assert.IsTrue(wf.steps[2] == st3);
 
         }
 
@@ -36,11 +36,11 @@ namespace UnitTestProject1
         [TestMethod]
         public void removeStepTest()
         {
-            AbstractWorkflow wf = new AbstractWorkflow();
+            Workflow wf = new Workflow();
 
-            AbstractStep st1 = new AbstractStep();
-            AbstractStep st2 = new AbstractStep();
-            AbstractStep st3 = new AbstractStep();
+            Step st1 = new Step();
+            Step st2 = new Step();
+            Step st3 = new Step();
 
             wf.addStep(st1);
             wf.addStep(st2);
@@ -50,7 +50,7 @@ namespace UnitTestProject1
             wf.removeLastStep();
             wf.removeLastStep();
 
-            Assert.IsTrue(wf.Step.Count == 0);
+            Assert.IsTrue(wf.steps.Count == 0);
 
         }
 
@@ -60,11 +60,11 @@ namespace UnitTestProject1
         [TestMethod]
         public void removeStepNextStepTest()
         {
-            AbstractWorkflow wf = new AbstractWorkflow();
+            Workflow wf = new Workflow();
 
-            AbstractStep st1 = new AbstractStep();
-            AbstractStep st2 = new AbstractStep();
-            AbstractStep st3 = new AbstractStep();
+            Step st1 = new Step();
+            Step st2 = new Step();
+            Step st3 = new Step();
 
             wf.addStep(st1);
             wf.addStep(st2);
@@ -72,7 +72,7 @@ namespace UnitTestProject1
 
             wf.removeLastStep();
 
-            Assert.IsTrue(wf.Step[1].nextSteps.Count == 0);
+            Assert.IsTrue(wf.steps[1].nextSteps.Count == 0);
         }
 
         /// <summary>
@@ -81,15 +81,15 @@ namespace UnitTestProject1
         [TestMethod]
         public void addStepNextStepTest()
         {
-            AbstractWorkflow wf = new AbstractWorkflow();
+            Workflow wf = new Workflow();
 
-            AbstractStep st1 = new AbstractStep();
-            AbstractStep st2 = new AbstractStep();
+            Step st1 = new Step();
+            Step st2 = new Step();
 
             wf.addStep(st1);
             wf.addStep(st2);
 
-            Assert.IsTrue(wf.Step[0].nextSteps[0] == st2);
+            Assert.IsTrue(wf.steps[0].nextSteps[0] == st2);
         }
     }
 }
