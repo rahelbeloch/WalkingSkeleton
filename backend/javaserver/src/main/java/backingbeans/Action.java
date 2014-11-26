72ba694f56642e4d7d069c2ca4565dd6b2501d8f
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 11:57:56 +0000

    #31 adapt backingbeans

--- backend/javaserver/src/main/java/backingbeans/Action.java
+++ backend/javaserver/src/main/java/backingbeans/Action.java
@@ -4,9 +4,9 @@ import abstractbeans.AbstractAction;
 
 public class Action extends AbstractAction{
 	
-	public Action(int id, int userId, String name){
+	public Action(int id, String username, String name){
 		super.name = name;
-		super.userId = userId; // shouldn't that be userName? DN
+		super.username = username;
 		super.setId(id);
 	}
 
--- backend/javaserver/src/main/java/backingbeans/StartStep.java
+++ backend/javaserver/src/main/java/backingbeans/StartStep.java
@@ -3,5 +3,8 @@ package backingbeans;
 import abstractbeans.AbstractStartStep;
 
 public class StartStep extends AbstractStartStep{
-
+	
+	public StartStep(String user){
+		this.username = user;
+	}
 }
