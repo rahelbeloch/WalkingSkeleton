1d7a888ce17b878f847247d81c1c37f73d53b694
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 13:57:29 +0000

    #31 removed exception print stackstrace, loggin necessary

--- backend/javaserver/src/main/java/moduledi/DIApp.java
+++ backend/javaserver/src/main/java/moduledi/DIApp.java
@@ -75,7 +75,7 @@ public class DIApp {
 					work = false;
 				}
 			} catch (IOException e) {
-				e.printStackTrace();
+				//Loggin
 			}
 			
 			
--- backend/javaserver/src/main/java/moduledi/ProduceData.java
+++ backend/javaserver/src/main/java/moduledi/ProduceData.java
@@ -35,8 +35,7 @@ public class ProduceData {
 		try {
 			p.addUser(user);
 		} catch (UserAlreadyExistsException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			// Loggin
 		}
 		countUser++;
 	}
