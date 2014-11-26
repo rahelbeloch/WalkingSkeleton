942daf58448cc7d022562e978f06d6c82acb8691
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 14:31:33 +0000

    #31 created new Interface to define communication between businessLogic and REST

---
+++ backend/javaserver/src/main/java/moduledi/logic.java
@@ -0,0 +1,44 @@
+package moduledi;
+
+import java.util.List;
+
+import abstractbeans.AbstractStep;
+import backingbeans.Action;
+import backingbeans.Item;
+import backingbeans.User;
+import backingbeans.Workflow;
+
+public interface logic {
+	
+	/*
+	 * workflow functions
+	 */
+	public void startWorkflow(int workflowID, User user);
+	public void addWorkflow(Workflow workflow); // later a workflows name will be available
+	public Workflow getWorkflow(int workflowID);
+	public void deleteWorkflow(int workflowID);
+
+	/*
+	 * item 
+	 */
+	public void stepOver(Item item, Action  action, User user);
+	
+	/*
+	 * step functions
+	 */
+	public void addStep (int workflowID, AbstractStep step);
+	public void deleteStep (int workflowID, int stepID); 
+	
+	/*
+	 * user functions
+	 */
+	public void addUser(String username);
+	public User getUser(String username);
+	public void deleteUser(String username);
+	
+	public List<Workflow> getWorkflowsByUser(User user);
+	public List<Item> getOpenItemsByUser(User user);
+	public List<Workflow> getStartableWorkflows(User user);
+	
+	
+}
