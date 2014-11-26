0efe223341772ff6eb521751b83f247e53c2d3fd
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-26 14:09:28 +0000

    #21 read URI out of server.config

--- backend/javaserver/src/main/java/restserver/RestServer.java
+++ backend/javaserver/src/main/java/restserver/RestServer.java
@@ -1,7 +1,11 @@
 package restserver;
 
+import java.io.BufferedInputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URI;
+import java.util.Properties;
 
 import org.glassfish.jersey.jdkhttp.JdkHttpServerFactory;
 import org.glassfish.jersey.server.ResourceConfig;
@@ -16,7 +20,7 @@ import com.sun.net.httpserver.HttpServer;
  */
 public class RestServer {
 
-	public static final String BASE_URI = "http://0.0.0.0:8080/";
+	private static String baseURI;
 	public static final int WARTEZEIT = 5;
 	
 	/**
@@ -24,15 +28,35 @@ public class RestServer {
 	 * @return the instance of the new server
 	 */
 	public static HttpServer startServer() {
+		Properties properties = new Properties();
+		BufferedInputStream stream;
+		//read configuration file for rest properties
+		try {
+			stream = new BufferedInputStream(new FileInputStream("server.config"));
+			properties.load(stream);
+			stream.close();
+		} catch (FileNotFoundException e) {
+			//TODO LOGGING
+		} catch (IOException e) {
+			//TODO LOGGING
+		} catch (SecurityException e) {
+			//TODO LOGGING
+		} 
+		baseURI = properties.getProperty("RestServerURI");
+		
         ResourceConfig rc = new ResourceConfig();
         rc.packages("restserver.resource");
-        return JdkHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        return JdkHttpServerFactory.createHttpServer(URI.create(baseURI), rc);
     }
+	
+	public String getBaseURI() {
+		return baseURI;
+	}
 	
 	public static void main(String[] args) throws IOException {
         final HttpServer server = startServer();
         System.out.println(String.format("Jersey app started with WADL available at "
-                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+                + "%sapplication.wadl\nHit enter to stop it...", baseURI));
         System.in.read();
         server.stop(WARTEZEIT);
     }
