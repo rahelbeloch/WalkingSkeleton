3c7a6a92295f324afc47fb070d11106997dc9b36
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-27 08:39:57 +0000

    #19 - changed modeltests based on the new modelstructure

--- backend/javaserver/src/test/java/de/hsrm/testswt02/modelTesting/ModelTests.java
+++ backend/javaserver/src/test/java/de/hsrm/testswt02/modelTesting/ModelTests.java
@@ -1,4 +1,4 @@
-package de.hsrm.testswt02.abstractbeansTesting;
+package de.hsrm.testswt02.modelTesting;
 
 import static org.hamcrest.CoreMatchers.instanceOf;
 import static org.junit.Assert.assertThat;
@@ -8,6 +8,8 @@ import de.hsrm.swt02.model.FinalStep;
 import de.hsrm.swt02.model.Item;
 import de.hsrm.swt02.model.MetaEntry;
 import de.hsrm.swt02.model.MetaState;
+import de.hsrm.swt02.model.RootElement;
+import de.hsrm.swt02.model.RootElementList;
 import de.hsrm.swt02.model.StartStep;
 import de.hsrm.swt02.model.Step;
 import de.hsrm.swt02.model.User;
@@ -15,57 +17,70 @@ import de.hsrm.swt02.model.Workflow;
 
 import org.junit.Test;
 
-public class AbstractBeansTest {
+public class ModelTests {
 	
 	@Test
-	public void abstractActionTest() {
-		Action a = new Action(1, "username", "test");
+	public void ActionTest() {
+		Action a = new Action(0, "usernametest", "desctest");
 		assertThat(a, instanceOf(Action.class));
 	}
 	
 	@Test
-	public void abstractFinalStepTest() {
+	public void FinalStepTest() {
 		FinalStep fs = new FinalStep();
 		assertThat(fs, instanceOf(FinalStep.class));
 	}
+	
 	@Test
-	public void abstractItemTest () {
+	public void ItemTest () {
 		Item i = new Item();
 		assertThat(i, instanceOf(Item.class));
 	}
 	
 	@Test
-	public void abstractMetaEntryTest() {
+	public void MetaEntryTest() {
 		MetaEntry me = new MetaEntry();
 		assertThat(me, instanceOf(MetaEntry.class));
 	}
 	
 	@Test
-	public void abstractMetaStateTest() {
+	public void MetaStateTest() {
 		MetaState ms = MetaState.valueOf("INACTIVE");
 		assertTrue(ms instanceof MetaState);
 	}
 	
 	@Test
-	public void abstractStartStepTest() {
-		StartStep ss = new StartStep();
+	public void RootElementTest() {
+		RootElement re = new RootElement();
+		assertTrue(re instanceof RootElement);
+	}
+	
+	@Test
+	public void RootElementListTest() {
+		RootElementList re = new RootElementList();
+		assertTrue(re instanceof RootElementList);
+	}
+	
+	@Test
+	public void StartStepTest() {
+		StartStep ss = new StartStep("usernametest");
 		assertThat(ss, instanceOf(StartStep.class));
 	}
 	
 	@Test
-	public void abstractStepTest() {
+	public void StepTest() {
 		Step s = new Step();
 		assertThat(s, instanceOf(Step.class));
 	}
 	
 	@Test
-	public void abstractUserTest() {
+	public void UserTest() {
 		User u = new User();
 		assertThat(u, instanceOf(User.class));
 	}
 	
 	@Test
-	public void abstractWorfklowTest() {
+	public void WorfklowTest() {
 		Workflow wf = new Workflow(0);
 		assertThat(wf, instanceOf(Workflow.class));
 	}
