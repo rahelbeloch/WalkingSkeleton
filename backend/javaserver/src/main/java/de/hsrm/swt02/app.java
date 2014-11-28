ee3c91b019190f4a5ee62189c1a662f6d0c1cf0e
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-28 10:33:28 +0000

    #46 -- added class commentary for app.class

--- backend/javaserver/src/main/java/de/hsrm/swt02/app.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/app.java
@@ -5,12 +5,13 @@ import com.google.inject.Injector;
 
 import de.hsrm.swt02.moduledi.SingleModule;
 
+/**Class for app start.
+ * Contains the main-method for the server application.
+ * Uses dependency injection.
+ */
 public class app {
 	public static void main(String[] args) {
 		Injector i = Guice.createInjector(new SingleModule());
-		// TODO executable main!!!
-		
-		
-			
+		// TODO executable main!!!	
 	}
 }
