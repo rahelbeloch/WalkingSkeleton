fc3d2765ef8b59a580e589e0111e79a124c936be
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-28 09:09:06 +0000

    #46 -- added dynamic log file names by using logFileName property from the server.config

--- backend/javaserver/src/main/java/de/hsrm/swt02/logging/LogConfigurator.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/logging/LogConfigurator.java
@@ -4,7 +4,9 @@ import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 import java.util.logging.ConsoleHandler;
 import java.util.logging.Formatter;
@@ -20,25 +22,34 @@ public class LogConfigurator {
 	
     static private FileHandler fileHTMLHandler;
     static private Formatter formatterHTML;
-    static private HashMap<String, Level> levelMap = new HashMap<>();
+    static private Map<String, Level> levelMap;
+    static {
+    	Map<String, Level> unmodifiableMap = new HashMap<String, Level>();
+    	unmodifiableMap.put("severe", Level.SEVERE);
+    	unmodifiableMap.put("warning", Level.WARNING);
+    	unmodifiableMap.put("info", Level.INFO);
+    	unmodifiableMap.put("config", Level.CONFIG);
+    	unmodifiableMap.put("fine", Level.FINE);
+    	unmodifiableMap.put("finer", Level.FINER);
+    	unmodifiableMap.put("finest", Level.FINEST);
+    	levelMap = Collections.unmodifiableMap(unmodifiableMap);
+    }
     
-    /**
-     * setup methode, use this ONE TIME to start the logger with configuration from server.config
+    /**Setup method to load the logger configuration from the 'server.config' file.
+     * Keep in mind that the configuration is a global setting for the UseLogger.class
+     * That means you have to call this method just once to setup your configuration.
      * @throws IOException
      */
     static public void setup() throws IOException {
         Logger logger = Logger.getLogger(UseLogger.class.getName());
-        levelMap.put("severe", Level.SEVERE);
-        levelMap.put("warning", Level.WARNING);
-        levelMap.put("info", Level.INFO);
-        levelMap.put("config", Level.CONFIG);
-        levelMap.put("fine", Level.FINE);
-        levelMap.put("finer", Level.FINER);
-        levelMap.put("finest", Level.FINEST);
         Properties properties = new Properties();
         BufferedInputStream stream;
+        // properties
+        Level logLevel = levelMap.get(properties.getProperty("LogLevel"));
+        String consoleLogging = properties.getProperty("ConsoleLogging");
+        String logFile = properties.getProperty("LogFile");
         
-        //read configuration file for logging properties
+        // read configuration file for logging properties
         try {
             stream = new BufferedInputStream(new FileInputStream("server.config"));
             properties.load(stream);
@@ -50,19 +61,20 @@ public class LogConfigurator {
         } catch (SecurityException e) {
             e.printStackTrace();
         }
-        
-        //read properties
-        Level logLevel = levelMap.get(properties.getProperty("LogLevel"));
-        String consoleLogging = properties.getProperty("ConsoleLogging");
-        
-        //default level for logging
+        logLevel = levelMap.get(properties.getProperty("LogLevel"));
+        consoleLogging = properties.getProperty("ConsoleLogging");
+        logFile = properties.getProperty("LogFile");
+
+        // default level for logging
         logger.setLevel(logLevel);
-        fileHTMLHandler = new FileHandler("log.html");
+        // set handler for a specific log file
+        fileHTMLHandler = new FileHandler(logFile);
+        // set HTML-Formatter for a logging handler
         formatterHTML = new HTMLFormatter();
         fileHTMLHandler.setFormatter(formatterHTML);
         logger.addHandler(fileHTMLHandler);
         
-        //disable console logging
+        // disable console logging
         if (consoleLogging.equals("false")) {
             Logger rootLogger = Logger.getLogger("");
             Handler[] handlers = rootLogger.getHandlers();
