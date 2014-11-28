b19eda2794232e34c8ba6ee9408d25a949f27e8b
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-28 11:52:50 +0000

    #46 -- fixed some checkstyle issues

---
+++ backend/javaserver/src/main/java/de/hsrm/swt02/App.java
@@ -0,0 +1,21 @@
+package de.hsrm.swt02;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import de.hsrm.swt02.moduledi.SingleModule;
+
+/**Class for application start.
+ * Contains the main-method for the server application.
+ * Uses dependency injection.
+ */
+public class App {
+        public static final Injector INJECTOR = Guice.createInjector(new SingleModule());
+        
+        /**Application startup method.
+         * @param args are the program start parameters
+         */
+        public static void main(String[] args) {
+             // TODO executable main!!!
+        }
+}
