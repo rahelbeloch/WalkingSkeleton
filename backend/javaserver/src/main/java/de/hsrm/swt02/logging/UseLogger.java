fc570f7d3ad81081126f645f55538a679d427712
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-11-28 12:30:14 +0000

    #46 suites checkstyle

--- backend/javaserver/src/main/java/de/hsrm/swt02/logging/UseLogger.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/logging/UseLogger.java
@@ -8,83 +8,83 @@ import java.util.logging.Level;
  *  The logger has a logging level which can be set to adjust the logger.
  */
 public class UseLogger {
-    private final static Logger LOGGER = Logger.getLogger(UseLogger.class.getName());
+        private final static Logger LOGGER = Logger.getLogger(UseLogger.class.getName());
     
-    /**Sets the logging level for the logger to the given log level.
-     * @param level is the value for the next logging operations
-     */
-    public void setLoggingLevel(Level level){
-    	LOGGER.setLevel(level);
-    }
+        /**Sets the logging level for the logger to the given log level.
+        * @param level is the value for the next logging operations
+        */
+        public void setLoggingLevel(Level level) {
+                LOGGER.setLevel(level);
+        }
     
-    /**Logs a message with logging level SEVERE.
-     * @param logMsg is the message to log
-     */
-    public void logSevere(String logMsg) {
-        LOGGER.severe(logMsg);
-    }
+        /**Logs a message with logging level SEVERE.
+        * @param logMsg is the message to log
+        */
+        public void logSevere(String logMsg) {
+                LOGGER.severe(logMsg);
+        }
     
-    /**Logs a message with logging level WARNING.
-     * @param logMsg is the message to log
-     */
-    public void logWarning(String logMsg) {
-    	LOGGER.warning(logMsg);
-    }
+        /**Logs a message with logging level WARNING.
+        * @param logMsg is the message to log
+        */
+        public void logWarning(String logMsg) {
+                LOGGER.warning(logMsg);
+        }
     
-    /**Logs a message with logging level INFO.
-     * @param logMsg is the message to log
-     */
-    public void logInfo(String logMsg) {
-    	LOGGER.info(logMsg);
-    }
+        /**Logs a message with logging level INFO.
+        * @param logMsg is the message to log
+        */
+        public void logInfo(String logMsg) {
+                LOGGER.info(logMsg);
+        }
     
-    /**Logs a message with logging level CONFIG.
-     * @param logMsg is the message to log
-     */
-    public void logConfig(String logMsg) {
-    	LOGGER.config(logMsg);
-    }
+        /**Logs a message with logging level CONFIG.
+        * @param logMsg is the message to log
+        */
+        public void logConfig(String logMsg) {
+                LOGGER.config(logMsg);
+        }
     
-    /**Logs a message with logging level FINE.
-     * @param logMsg is the message to log
-     */
-    public void logFine(String logMsg) {
-    	LOGGER.fine(logMsg);
-    }
+        /**Logs a message with logging level FINE.
+        * @param logMsg is the message to log
+        */
+        public void logFine(String logMsg) {
+                LOGGER.fine(logMsg);
+        }
     
-    /**Logs a message with logging level FINER.
-     * @param logMsg is the message to log
-     */
-    public void logFiner(String logMsg) {
-    	LOGGER.finer(logMsg);
-    }
+        /**Logs a message with logging level FINER.
+        * @param logMsg is the message to log
+        */
+        public void logFiner(String logMsg) {
+                LOGGER.finer(logMsg);
+        }
     
-    /**Logs a message with logging level FINEST.
-     * @param logMsg is the message to log
-     */
-    public void logFinest(String logMsg) {
-    	LOGGER.finest(logMsg);
-    }
+        /**Logs a message with logging level FINEST.
+        * @param logMsg is the message to log
+        */
+        public void logFinest(String logMsg) {
+                LOGGER.finest(logMsg);
+        }
     
-    /**Logs a message String into the logging file.
-     * The logging file is specified in the 'server.config' file.
-     * If the specified log level is lower than the current log level,
-     * the message wont be logged.
-     * @param logLevel is the urgency level for log message
-     * @param logMsg is the message that should be logged
-     */
-    public void log(Level logLevel, String logMsg){
-    	LOGGER.log(logLevel, logMsg);
-    }
+        /**Logs a message String into the logging file.
+        * The logging file is specified in the 'server.config' file.
+        * If the specified log level is lower than the current log level,
+        * the message wont be logged.
+        * @param logLevel is the urgency level for log message
+        * @param logMsg is the message that should be logged
+        */
+        public void log(Level logLevel, String logMsg) {
+                LOGGER.log(logLevel, logMsg);
+        }
     
-    /**Logs an exception message into the logging file.
-     * The logging file is specified in the 'server.config' file.
-     * If the specified log level is lower than the current log level,
-     * the message wont be logged.
-     * @param logLevel is the urgency level for log message
-     * @param throwable is the source for the log information
-     */
-    public void log(Level logLevel, Throwable throwable){
-    	LOGGER.log(logLevel, "", throwable);
-    }
+        /**Logs an exception message into the logging file.
+        * The logging file is specified in the 'server.config' file.
+        * If the specified log level is lower than the current log level,
+        * the message wont be logged.
+        * @param logLevel is the urgency level for log message
+        * @param throwable is the source for the log information
+        */
+        public void log(Level logLevel, Throwable throwable) {
+                LOGGER.log(logLevel, "", throwable);
+        }
 }
