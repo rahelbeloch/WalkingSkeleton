4398abb1f625a75c12fdb011e463ea54274104dd
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 15:33:36 +0000

    #21 main method removed from rest server

--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/RestServer.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/RestServer.java
@@ -67,10 +67,4 @@ public class RestServer {
 	public String getBaseURI() {
 		return baseURI;
 	}
-	
-	public static void main(String [] args) {
-		RestServer server = new RestServer();
-		server.startHTTPServer();
-	}
-
 }
