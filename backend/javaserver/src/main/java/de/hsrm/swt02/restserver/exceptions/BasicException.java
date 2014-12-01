e3bf04a33ad3e28845978d9142987824336c7b26
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 16:49:35 +0000

    #39 -- new exceptions package including BasicException

---
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/BasicException.java
@@ -0,0 +1,20 @@
+package de.hsrm.swt02.restserver.exceptions;
+
+public class BasicException extends Exception {
+    
+    private static final long serialVersionUID = 1234535084952400723L;
+    private int errorcode = 10000;
+    
+    public BasicException() {
+        super();
+    }
+
+    public BasicException(String msg) {
+        super(msg);
+    }
+    
+    public int getErrorCode() {
+        return this.errorcode;
+    }
+
+}
