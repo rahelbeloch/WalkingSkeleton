55cd4d7832d8f2d1274a44d573adbd1f4370a462
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 10:07:06 +0000

    #34 fixed messaging unit tests

--- backend/javaserver/src/test/java/de/hsrm/testswt02/messagingtest/TestMessagingListener.java
+++ backend/javaserver/src/test/java/de/hsrm/testswt02/messagingtest/TestMessagingListener.java
@@ -13,7 +13,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 
 /**
  * This class is for testing. It simulates a message listener listening to a
- * Test_Topic.
+ * TEST_TOPIC.
  */
 public class TestMessagingListener implements MessageListener {
     private Connection connection;
@@ -21,45 +21,65 @@ public class TestMessagingListener implements MessageListener {
     private MessageConsumer consumer;
     private String receivedMsg = "fail";
 
-    private String BROKERURL = "tcp://localhost:61616";
-    final private String TOPIC_NAME = "TEST_TOPIC";
-
+    private static final String BROKERURL = "tcp://localhost:61616";
+    private static final String TOPIC_NAME = "TEST_TOPIC";
+    
+    /**
+     * Constructor.
+     */
     public TestMessagingListener() {
 
     }
-
+    
+    /**
+     * Call-back method for messages.
+     * Called after incoming messages.
+     * @param message incoming message
+     */
     public void onMessage(Message message) {
-        TextMessage tm = (TextMessage) message;
+        final TextMessage tm = (TextMessage) message;
         try {
             receivedMsg = tm.getText();
         } catch (JMSException e) {
-
+            receivedMsg = null;
         }
     }
 
+    /**
+     * Starts listening.
+     */
     public void start() {
-        ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
+        final ActiveMQConnectionFactory factory = new ActiveMQConnectionFactory(
                 BROKERURL);
         try {
             connection = factory.createConnection();
             session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-            Topic topic = session.createTopic(TOPIC_NAME);
+            final Topic topic = session.createTopic(TOPIC_NAME);
             consumer = session.createConsumer(topic);
             consumer.setMessageListener(this);
             connection.start();
         } catch (JMSException e) {
-
+            e.printStackTrace();
         }
-    }
 
+    }
+    
+    /**
+     * Stops listening.
+     */
     public void stop() {
         try {
             connection.close();
         } catch (JMSException e) {
-
+            e.printStackTrace();
         }
     }
-
+    
+    /**
+     * Returns the last received string message.
+     * 
+     * @return the received string.
+     */
     public String getReceivedMsg() {
         return receivedMsg;
     }
