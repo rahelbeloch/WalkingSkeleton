6016ae2134ff59248dccce8f38c232c97732e98f
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 12:31:13 +0000

    #31 -- format code

--- backend/javaserver/src/main/java/de/hsrm/swt02/constructionfactory/ConstructionFactory.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/constructionfactory/ConstructionFactory.java
@@ -7,30 +7,35 @@ import de.hsrm.swt02.businesslogic.Logic;
 import de.hsrm.swt02.messaging.ServerPublisher;
 
 /**
- * This class provides instances of the Logic interface and Messaging operations.
+ * This class provides instances of the Logic interface and Messaging
+ * operations.
  *
  */
 public class ConstructionFactory {
-	
-	private static final Injector INJECTOR = Guice.createInjector(new SingleModule());
-	
-	private static Logic logic = INJECTOR.getInstance(Logic.class);
-	private static ServerPublisher serverPublisher = INJECTOR.getInstance(ServerPublisher.class);
-	
-	/**
-	 * This method returns the Logic instance.
-	 * @return logic 
-	 */
-	public static Logic getLogic() {
-		return logic;
-	}
-	
-	/**
-	 * This method returns the instance of ServerPublisher.
-	 * @return serverpublisher
-	 */
-	public static ServerPublisher getPublisher() {
-		return serverPublisher;
-	}
+
+    private static final Injector INJECTOR = Guice
+            .createInjector(new SingleModule());
+
+    private static Logic logic = INJECTOR.getInstance(Logic.class);
+    private static ServerPublisher serverPublisher = INJECTOR
+            .getInstance(ServerPublisher.class);
+
+    /**
+     * This method returns the Logic instance.
+     * 
+     * @return logic
+     */
+    public static Logic getLogic() {
+        return logic;
+    }
+
+    /**
+     * This method returns the instance of ServerPublisher.
+     * 
+     * @return serverpublisher
+     */
+    public static ServerPublisher getPublisher() {
+        return serverPublisher;
+    }
 
 }
