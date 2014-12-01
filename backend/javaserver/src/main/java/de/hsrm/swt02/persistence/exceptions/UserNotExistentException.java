7c24b4c1f4f249e8775531770e59b0a814a9a126
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 16:50:33 +0000

    #39 -- renewed exceptions structure and completed exception package including PersistenceExceptions

--- backend/javaserver/src/main/java/de/hsrm/swt02/persistence/exceptions/UserNotExistentException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/persistence/exceptions/UserNotExistentException.java
@@ -1,14 +1,19 @@
 package de.hsrm.swt02.persistence.exceptions;
 
-public class UserNotExistentException extends Exception {
+public class UserNotExistentException extends NotExistentException {
 
     private static final long serialVersionUID = 4657473446452493460L;
-    int errorcode = 11251;
+    private int errorcode = 11251;
+    
     public UserNotExistentException() {
         super();
     }
 
     public UserNotExistentException(String msg) {
         super(msg);
+    }
+    
+    public int getErrorCode() {
+        return this.errorcode;
     }
 }
