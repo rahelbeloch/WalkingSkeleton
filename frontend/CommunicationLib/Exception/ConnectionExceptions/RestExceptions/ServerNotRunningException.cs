75db97f3084cef6183226a1bbfa84fc959973273
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 12:11:00 +0000

    #40 add ServerNotRunningException

---
+++ frontend/CommunicationLib/Exception/ConnectionExceptions/RestExceptions/ServerNotRunningException.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Exception.ConnectionExceptions.RestExceptions
+{
+    class ServerNotRunningException : RestException
+    {
+        private int _number = 12210;
+        public int number { get { return _number; } }
+
+        public ServerNotRunningException()
+        {
+
+        }
+
+        public ServerNotRunningException(string message)
+         : base(message)
+        {
+        }
+
+        public ServerNotRunningException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
