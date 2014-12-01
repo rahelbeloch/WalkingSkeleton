15132a19ff2efa928fb86321bbab3196e7baef1b
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-01 12:24:29 +0000

    #40 fixed

--- frontend/CommunicationLib/Exception/LogicExceptions/LogInExceptions/LogInException.cs
+++ frontend/CommunicationLib/Exception/LogicExceptions/LogInExceptions/LogInException.cs
@@ -8,18 +8,21 @@ namespace CommunicationLib.Exception
 {
     class LogInException : LogicException
     {
+        private int _number = 11100;
+        public int number { get { return _number; } }
+
         public LogInException()
         {
 
         }
 
-        public LogInException(string message, int number)
-         : base(message, number)
+        public LogInException(string message)
+         : base(message)
         {
         }
 
-        public LogInException(string message, System.Exception inner, int number)
-            : base(message, inner, number)
+        public LogInException(string message, System.Exception inner)
+            : base(message, inner)
         {
         }
     }
--- frontend/CommunicationLib/Exception/LogicExceptions/PersistenceExceptions/DoesntExistsException.cs
+++ frontend/CommunicationLib/Exception/LogicExceptions/PersistenceExceptions/DoesntExistsException.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace CommunicationLib.Exception
 {
-    class DoesntExistsException
+    class DoesntExistsException : PersistenceException
     {
+        private int _number = 11250;
+        public int number { get { return _number; } }
+
+        public DoesntExistsException()
+        {
+
+        }
+
+        public DoesntExistsException(string message)
+         : base(message)
+        {
+        }
+
+        public DoesntExistsException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
     }
 }
