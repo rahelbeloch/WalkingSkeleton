63bad7228d7e318270d25518b8350a878e5e05a3
khils001 <khils001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-02 17:29:42 +0000

    #32 create own togglebuttonclass

---
+++ frontend/UserClient/View/MyToggleButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls.Primitives;
+
+namespace UserClient.View
+{
+    public class MyToggleButton : ToggleButton
+    {
+        public int stepId{get; set;}
+        public int itemId { get; set; }
+        public string username { get; set; }
+
+        public MyToggleButton()
+            : base()
+        {
+
+        }
+    }
+}
