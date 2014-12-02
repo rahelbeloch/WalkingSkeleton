d8f9304fed76207d3d2acc482b8934b5cb2198f3
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-02 10:45:33 +0000

    #39 -- CodeStyle in package businesslogic.exceptions

--- backend/javaserver/src/main/java/de/hsrm/swt02/businesslogic/exceptions/LogicException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/businesslogic/exceptions/LogicException.java
@@ -4,7 +4,10 @@ import de.hsrm.swt02.restserver.exceptions.BasicException;
 
 public class LogicException extends BasicException {
     
-    private static final long serialVersionUID = 3428135084952400723L;
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -7612098314265342947L;
     private int errorcode = 11000;
     
     public LogicException() {
