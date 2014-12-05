9eafff7199fdbd4ee0179638e7ccd3a0868ab468
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-05 12:21:52 +0000

    #73 added new admin project (for refactoring)

---
+++ frontend/Admin/ViewModel/MainViewModel.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace Admin.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentView { get; set; }
+        
+        
+        private WorkflowViewModel _workflowViewModel = new WorkflowViewModel();
+        public WorkflowViewModel workflowViewModel { get { return _workflowViewModel; } }
+
+        private UserViewModel _userViewModel = new UserViewModel();
+        public UserViewModel userViewModel { get { return _userViewModel; }  }
+        
+        public MainViewModel()
+        {
+            PageViewModels.Add(userViewModel);
+            PageViewModels.Add(workflowViewModel);
+
+            CurrentPageViewModel = PageViewModels[0];
+        }
+
+
+        #region Fields
+ 
+        private ICommand _changePageCommand;
+ 
+        private ViewModelBase _currentPageViewModel;
+        private List<ViewModelBase> _pageViewModels;
+ 
+        #endregion
+ 
+ 
+        #region Properties / Commands
+ 
+        public ICommand ChangePageCommand
+        {
+            get
+            {
+                if (_changePageCommand == null)
+                {
+                    _changePageCommand = new ActionCommand(
+                        p => ChangeViewModel((ViewModelBase)p),
+                        p => p is ViewModelBase);
+                }
+                return _changePageCommand;
+            }
+        }
+ 
+        public List<ViewModelBase> PageViewModels
+        {
+            get
+            {
+                if (_pageViewModels == null)
+                    _pageViewModels = new List<ViewModelBase>();
+ 
+                return _pageViewModels;
+            }
+        }
+ 
+        public ViewModelBase CurrentPageViewModel
+        {
+            get
+            {
+                return _currentPageViewModel;
+            }
+            set
+            {
+                if (_currentPageViewModel != value)
+                {
+                    _currentPageViewModel = value;
+                    OnChanged("CurrentPageViewModel");
+                }
+            }
+        }
+ 
+        #endregion
+ 
+        #region Methods
+
+        private void ChangeViewModel(ViewModelBase viewModel)
+        {
+            Console.WriteLine("change view model...");
+            if (!PageViewModels.Contains(viewModel))
+                PageViewModels.Add(viewModel);
+ 
+            CurrentPageViewModel = PageViewModels
+                .FirstOrDefault(vm => vm == viewModel);
+        }
+ 
+        #endregion
+    }
+}
