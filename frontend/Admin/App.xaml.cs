378e57e1dad61206c0afd45c56186d87bf7591e5
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-05 12:21:52 +0000

    #73 added new admin project (for refactoring)

---
+++ frontend/Admin/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Admin.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Admin
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow app = new MainWindow();
+            MainViewModel context = new MainViewModel();
+            app.DataContext = context;
+            app.Show();
+        }
+    }
+}
