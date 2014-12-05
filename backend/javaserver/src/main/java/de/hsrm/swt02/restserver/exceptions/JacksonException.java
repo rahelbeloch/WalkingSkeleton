095bda6488de0d55b91e35ac5d1f7ad782d8b91e
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-05 09:50:22 +0000

    #30 Exception handling improved

---
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/JacksonException.java
@@ -0,0 +1,24 @@
+package de.hsrm.swt02.restserver.exceptions;
+
+public class JacksonException extends RestServerException {
+
+
+    /**
+     * 
+     */
+    private static final long serialVersionUID = 5071158186141166613L;
+    private int errorcode = 12210;
+    
+    public JacksonException() {
+        super();
+    }
+
+    public JacksonException(String msg) {
+        super(msg);
+    }
+    
+    public int getErrorCode() {
+        return this.errorcode;
+    }
+    
+}
