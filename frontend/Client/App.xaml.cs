df5b8bb131a07e3efd56e73da00e6e7281a5f6e4
khils001 <khils001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-07 14:41:31 +0000

    #29 init client project with new structure

---
+++ frontend/Client/App.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using Client.ViewModel;
+
+namespace Client
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow app = new MainWindow();
+            MainViewModel context = new MainViewModel();
+            app.DataContext = context;
+            app.Show();
+        }
+    }
+}
