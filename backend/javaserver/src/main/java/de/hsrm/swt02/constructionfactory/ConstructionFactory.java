e70ffcd520fdd53655abb1c72bfe34c8778e9f7d
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-08 08:59:09 +0000

    #31 Improved usage of the ConstructionFactory by adding lazy loading pattern

--- backend/javaserver/src/main/java/de/hsrm/swt02/constructionfactory/ConstructionFactory.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/constructionfactory/ConstructionFactory.java
@@ -1,49 +1,69 @@
 package de.hsrm.swt02.constructionfactory;
 
+import java.util.logging.Level;
+
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
 import de.hsrm.swt02.businesslogic.Logic;
+import de.hsrm.swt02.logging.UseLogger;
 import de.hsrm.swt02.messaging.ServerPublisher;
 import de.hsrm.swt02.messaging.ServerPublisherBrokerException;
 
 /**
  * This class provides instances of the Logic interface and Messaging
  * operations.
- *
+ * Uses a Singleton pattern.
  */
 public class ConstructionFactory {
 
     private static final Injector INJECTOR = Guice
             .createInjector(new SingleModule());
-
-    private static Logic logic = INJECTOR.getInstance(Logic.class);
-    private static ServerPublisher serverPublisher = INJECTOR
-            .getInstance(ServerPublisher.class);
     
-    public ConstructionFactory() {
+    private static Logic logic; 
+    private static ServerPublisher serverPublisher;
+    private static ConstructionFactory theInstance;
+
+    /**
+     * Constructor.
+     */
+    private ConstructionFactory() {
+        logic = INJECTOR.getInstance(Logic.class);
+        serverPublisher = INJECTOR.getInstance(ServerPublisher.class);
         try {
             serverPublisher.startBroker();
         } catch (ServerPublisherBrokerException e) {
-            //TODO find a solution for the logging instance-problem
+            final UseLogger logger = new UseLogger();
+            logger.log(Level.SEVERE, e);
         }
+    }
+    
+    /**
+     * Return the instance for this class.
+     * @return the instance
+     */
+    public static ConstructionFactory getInstance() {
+        if (theInstance == null) {
+            return new ConstructionFactory();
+        }
+        return theInstance;
     }
 
     /**
      * This method returns the Logic instance.
      * 
-     * @return logic
+     * @return logic the instance responsible for server logic
      */
-    public static Logic getLogic() {
+    public Logic getLogic() {
         return logic;
     }
 
     /**
      * This method returns the instance of ServerPublisher.
      * 
-     * @return serverpublisher
+     * @return serverPublisher the instance responsible for messaging purpose
      */
-    public static ServerPublisher getPublisher() {
+    public ServerPublisher getPublisher() {
         return serverPublisher;
     }
 
