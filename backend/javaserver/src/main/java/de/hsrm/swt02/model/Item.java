a51cffa3df738fee433809300bd6688278fac09f
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-08 11:46:19 +0000

    #51 -- extended getActStep method

--- backend/javaserver/src/main/java/de/hsrm/swt02/model/Item.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/model/Item.java
@@ -6,203 +6,228 @@ import java.util.List;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
 /**
- * This class represents an Item. It extends the class RootElement, so it can have an Id.
+ * This class represents an Item. It extends the class RootElement, so it can
+ * have an Id.
  */
 public class Item extends RootElement {
-    // Used for (de)serialization. Do not change.
-    private int workflowId;
+	// Used for (de)serialization. Do not change.
+	private int workflowId;
 
-    // Used for (de)serialization. Do not change.
-    private List<MetaEntry> metadata;
+	// Used for (de)serialization. Do not change.
+	private List<MetaEntry> metadata;
 
-    // Used for (de)serialization. Do not change.
-    private boolean finished;
+	// Used for (de)serialization. Do not change.
+	private boolean finished;
 
-    // When an Item object is (de)serializated: state will be ignored because in the client item, there is no state.
-    @JsonIgnore
-    private String state;
+	// When an Item object is (de)serializated: state will be ignored because in
+	// the client item, there is no state.
+	@JsonIgnore
+	private String state;
 
-    /**
-     * Constructor for Item.
-     */
-    public Item() {
-        super();
-        metadata = new ArrayList<MetaEntry>();
-    }
+	/**
+	 * Constructor for Item.
+	 */
+	public Item() {
+		super();
+		metadata = new ArrayList<MetaEntry>();
+	}
 
-    /**
-     * WorkflowId getter.
-     * @return workflowId id of the workflow
-     */
-    public int getWorkflowId() {
-        return workflowId;
-    }
+	/**
+	 * WorkflowId getter.
+	 * 
+	 * @return workflowId id of the workflow
+	 */
+	public int getWorkflowId() {
+		return workflowId;
+	}
 
-    /**
-     * WorkflowId setter.
-     * @param workflowId id of the workflow
-     */
-    public void setWorkflowId(int workflowId) {
-        this.workflowId = workflowId;
-    }
+	/**
+	 * WorkflowId setter.
+	 * 
+	 * @param workflowId
+	 *            id of the workflow
+	 */
+	public void setWorkflowId(int workflowId) {
+		this.workflowId = workflowId;
+	}
 
-    /**
-     * Metadata (list of MetaEntries) getter.
-     * @return metadata list of metadata
-     */
-    public List<MetaEntry> getMetadata() {
-        return this.metadata;
-    }
+	/**
+	 * Metadata (list of MetaEntries) getter.
+	 * 
+	 * @return metadata list of metadata
+	 */
+	public List<MetaEntry> getMetadata() {
+		return this.metadata;
+	}
 
-    /**
-     * Finished getter.
-     * @return finished true of false wether or not the workflow is finished
-     */
-    public boolean isFinished() {
-        return finished;
-    }
+	/**
+	 * Finished getter.
+	 * 
+	 * @return finished true of false wether or not the workflow is finished
+	 */
+	public boolean isFinished() {
+		return finished;
+	}
 
-    /**
-     * Finished setter.
-     * @param finished finished true of false wether or not the workflow is finished
-     */
-    public void setFinished(boolean finished) {
-        this.finished = finished;
-    }
-    
-    /**
-     * State getter.
-     * @return state
-     */
-    public String getState() {
-        return state;
-    }
+	/**
+	 * Finished setter.
+	 * 
+	 * @param finished
+	 *            finished true of false wether or not the workflow is finished
+	 */
+	public void setFinished(boolean finished) {
+		this.finished = finished;
+	}
 
-    /**
-     * State setter.
-     * @param state is a string representing the state of a step
-     */
-    public void setState(String state) {
-        this.state = state;
-    }
+	/**
+	 * State getter.
+	 * 
+	 * @return state
+	 */
+	public String getState() {
+		return state;
+	}
 
-    /**
-     * This method gets an entry of the metadata list which suits the group and
-     * key parameters.
-     * 
-     * @param key is the id of an entry
-     * @param group is the type of an entry
-     * @return the suitable entry, else return NULL
-     */
-    public MetaEntry getEntry(String key, String group) {
+	/**
+	 * State setter.
+	 * 
+	 * @param state
+	 *            is a string representing the state of a step
+	 */
+	public void setState(String state) {
+		this.state = state;
+	}
 
-        for (MetaEntry a : metadata) {
-            if (a.getGroup().equals(group) && a.getKey().equals(key)) {
-                return a;
-            }
-        }
-        return null;
-    }
+	/**
+	 * This method gets an entry of the metadata list which suits the group and
+	 * key parameters.
+	 * 
+	 * @param key
+	 *            is the id of an entry
+	 * @param group
+	 *            is the type of an entry
+	 * @return the suitable entry, else return NULL
+	 */
+	public MetaEntry getEntry(String key, String group) {
 
-    /**
-     * This method gets the value of an entry.
-     * 
-     * @param key is the id of an entry
-     * @param group is the type of an entry
-     * @return the suitable entry value, if the entry was not found return NULL
-     */
-    public String getEntryValue(String key, String group) {
+		for (MetaEntry a : metadata) {
+			if (a.getGroup().equals(group) && a.getKey().equals(key)) {
+				return a;
+			}
+		}
+		return null;
+	}
 
-        final MetaEntry ame = getEntry(key, group);
+	/**
+	 * This method gets the value of an entry.
+	 * 
+	 * @param key
+	 *            is the id of an entry
+	 * @param group
+	 *            is the type of an entry
+	 * @return the suitable entry value, if the entry was not found return NULL
+	 */
+	public String getEntryValue(String key, String group) {
 
-        if (ame != null) {
-            return ame.getValue();
-        }
-        return null;
-    }
+		final MetaEntry ame = getEntry(key, group);
 
-    /**
-     * This method returns the Metastate of an entry.
-     * 
-     * @param key is the id of an entry
-     * @return the Metastate of the searched entry
-     */
-    public String getStepState(int key) {
+		if (ame != null) {
+			return ame.getValue();
+		}
+		return null;
+	}
 
-        return MetaState.fromValue(getEntryValue(key + "", "step")).toString();
-    }
+	/**
+	 * This method returns the Metastate of an entry.
+	 * 
+	 * @param key
+	 *            is the id of an entry
+	 * @return the Metastate of the searched entry
+	 */
+	public String getStepState(int key) {
 
-    /**
-     * This method gets a list which contains looked for entries.
-     * 
-     * @param group is the type of the entries which are looked for
-     * @return a list of suitable entries
-     */
-    public List<MetaEntry> getForGroup(String group) {
+		return MetaState.fromValue(getEntryValue(key + "", "step")).toString();
+	}
 
-        final List<MetaEntry> list = new ArrayList<MetaEntry>();
+	/**
+	 * This method gets a list which contains looked for entries.
+	 * 
+	 * @param group
+	 *            is the type of the entries which are looked for
+	 * @return a list of suitable entries
+	 */
+	public List<MetaEntry> getForGroup(String group) {
 
-        for (MetaEntry a : metadata) {
-            if (a.getGroup().equals(group)) {
-                list.add(a);
-            }
-        }
-        return list;
-    }
+		final List<MetaEntry> list = new ArrayList<MetaEntry>();
 
-    /**
-     * returns the MetaEntry of the current step.
-     * @return me or null
-     */
-    public MetaEntry getActStep() {
+		for (MetaEntry a : metadata) {
+			if (a.getGroup().equals(group)) {
+				list.add(a);
+			}
+		}
+		return list;
+	}
+
+	/**
+	 * returns the MetaEntry of the current step.
+	 * 
+	 * @return me or null
+	 */
+	public MetaEntry getActStep() {
         for (MetaEntry me : getForGroup("step")) {
-            if ((me.getValue()).equals(MetaState.OPEN.toString())) {
+            if ((me.getValue().equals(MetaState.OPEN.toString())) || me.getValue().equals(MetaState.BUSY.toString())) {
                 return me;
             }
         }
         return null;
     }
 
-    /**
-     * This method sets the value of an entry or adds a new entry.
-     * 
-     * @param key is the id of an entry
-     * @param group is the type of an entry
-     * @param value represents an entrie's content
-     */
-    public void set(String key, String group, String value) {
+	/**
+	 * This method sets the value of an entry or adds a new entry.
+	 * 
+	 * @param key
+	 *            is the id of an entry
+	 * @param group
+	 *            is the type of an entry
+	 * @param value
+	 *            represents an entrie's content
+	 */
+	public void set(String key, String group, String value) {
 
-        final MetaEntry ame = getEntry(key, group);
+		final MetaEntry ame = getEntry(key, group);
 
-        if (ame != null) {
-            ame.setValue(value);
-        } else {
-            final MetaEntry entry = new MetaEntry();
-            entry.setGroup(group);
-            entry.setKey(key);
-            entry.setValue(value);
-            metadata.add(entry);
-        }
-    }
+		if (ame != null) {
+			ame.setValue(value);
+		} else {
+			final MetaEntry entry = new MetaEntry();
+			entry.setGroup(group);
+			entry.setKey(key);
+			entry.setValue(value);
+			metadata.add(entry);
+		}
+	}
 
-    /**
-     * This method sets specifically the metastate of an step entry.
-     * 
-     * @param key is the id of an entry
-     * @param value is the new state of an entry
-     */
-    public void setStepState(int key, String value) {
+	/**
+	 * This method sets specifically the metastate of an step entry.
+	 * 
+	 * @param key
+	 *            is the id of an entry
+	 * @param value
+	 *            is the new state of an entry
+	 */
+	public void setStepState(int key, String value) {
 
-        set(Integer.toString(key), "step", value);
-    }
+		set(Integer.toString(key), "step", value);
+	}
 
-    /**
-     * This methods is just for the initial state setting of an step.
-     * 
-     * @param value has to be OPEN, for enabling very first Step
-     */
-    public void setFirstStepState(String value) {
+	/**
+	 * This methods is just for the initial state setting of an step.
+	 * 
+	 * @param value
+	 *            has to be OPEN, for enabling very first Step
+	 */
+	public void setFirstStepState(String value) {
 
-        getForGroup("step").get(0).setValue(value);
-    }
+		getForGroup("step").get(0).setValue(value);
+	}
 }
