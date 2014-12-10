7057f7c002e33a84fadcdb646c0d4967015ba88d
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-10 13:25:58 +0000

    # add documentation

--- frontend/CommunicationLib/Model/Exception/LogicExceptions/LogicException.cs
+++ frontend/CommunicationLib/Model/Exception/LogicExceptions/LogicException.cs
@@ -8,6 +8,10 @@ namespace CommunicationLib.Exception
 {
     public class LogicException : BasicException
     {
+        /// <summary>
+        /// This Exception es a child of BasicException. 
+        /// Each Exception has an number, here it is 11000. 
+        /// </summary>
         private int _number = 11000;
         new public int number { get { return _number; } }
 
@@ -17,11 +21,20 @@ namespace CommunicationLib.Exception
 
         }
 
+         /// <summary>
+         /// This constructor allows to add a spezial message.
+         /// </summary>
+         /// <param name="message">the message</param>
         public LogicException(string message)
          : base(message)
         {
         }
 
+        /// <summary>
+        /// This constructor allows to add a spezial message and an other exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
         public LogicException(string message, System.Exception inner)
             : base(message, inner)
         {
