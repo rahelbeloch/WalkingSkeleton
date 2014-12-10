304e525d31b272241957878c65d5155cf926e446
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-10 17:33:19 +0000

    #40 added some Exceptions

---
+++ frontend/CommunicationLib/Model/Exception/LogicExceptions/NoPermissionException/ItemNotForwardableException.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Exception
+{
+    class ItemNotForwardableException : NoPermissionException
+    {
+        private int _number = 11320;
+        new public int number { get { return _number; } }
+
+        public ItemNotForwardable()
+            : base("Das Item ist nicht weiterschaltbar.")
+        {
+
+        }
+
+        public ItemNotForwardable(string message)
+            : base(message)
+        {
+        }
+
+        public ItemNotForwardable(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
