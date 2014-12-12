73c842f0060dab663260dd208ecda1560f5eca1b
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-12 11:45:33 +0000

    #64 added Javadoc to RoleResource

--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/resource/RoleResource.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/resource/RoleResource.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.logging.Level;
 
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
@@ -24,10 +25,8 @@ import de.hsrm.swt02.logging.UseLogger;
 import de.hsrm.swt02.messaging.ServerPublisher;
 import de.hsrm.swt02.messaging.ServerPublisherBrokerException;
 import de.hsrm.swt02.model.Role;
-import de.hsrm.swt02.model.User;
 import de.hsrm.swt02.persistence.exceptions.RoleAlreadyExistsException;
 import de.hsrm.swt02.persistence.exceptions.RoleNotExistentException;
-import de.hsrm.swt02.persistence.exceptions.UserAlreadyExistsException;
 import de.hsrm.swt02.restserver.LogicResponse;
 import de.hsrm.swt02.restserver.Message;
 import de.hsrm.swt02.restserver.exceptions.JacksonException;
@@ -42,6 +41,12 @@ public class RoleResource {
     public static final String PREFIX = "[restserver] ";
     LogicResponse logicResponse;
     
+    /**
+     * 
+     * This Method grants the Clients access to all Roles stored in persistence
+     * 
+     * @return All Roles in the persistence as string if successful, 500 Server Error if not
+     */
     @GET
     @Path("roles")
     @Produces(MediaType.TEXT_PLAIN)
@@ -68,6 +73,13 @@ public class RoleResource {
         return Response.ok(rolesAsString).build();
     }
     
+    /**
+     * 
+     * This Method enables Clients to save Roles into the persistence
+     * 
+     * @param formParams the role to be saved is available via key "data"
+     * @return 200 OK if successful, 500 Server Error if not
+     */
     @POST
     @Path("role")
     @Produces(MediaType.TEXT_PLAIN)
@@ -104,6 +116,14 @@ public class RoleResource {
         return Response.ok("Role stored").build();
     }
     
+    /**
+     * 
+     * This Method enables Clients to update existing Roles
+     * 
+     * @param rolename the name of the Role to be updated
+     * @param formParams the key "data" holds the instance of Role to be saved
+     * @return 200 OK if successful, 500 Server Error if not
+     */
     @PUT
     @Path("role/{rolename}")
     @Produces(MediaType.TEXT_PLAIN)
@@ -138,6 +158,39 @@ public class RoleResource {
             }
         }
         LOGGER.log(Level.INFO, loggingBody + " Role successfully updated.");
+        return Response.ok().build();
+    }
+    
+    /**
+     * 
+     * This method enables Clients to delete Roles from the persistence
+     * 
+     * @param rolename the name of the role to be deleted
+     * @return 200 OK if successful, 500 ServerError if not
+     */
+    @DELETE
+    @Path("role/{rolename}")
+    @Produces(MediaType.TEXT_PLAIN)
+    public Response deleteUser(@PathParam("rolename") String rolename) {
+        final String loggingBody = PREFIX + "DELETE /resource/role/" + rolename;
+        LOGGER.log(Level.INFO, loggingBody);
+        
+        try {
+            // TODO: get Role from logic, serialize it and send it with answer
+            logicResponse = LOGIC.deleteRole(rolename);
+        } catch (RoleNotExistentException e1) {
+            LOGGER.log(Level.INFO,e1);
+            return Response.serverError().entity(String.valueOf(e1.getErrorCode())).build();
+        }
+        
+        for (Message m : logicResponse.getMessages()) {
+            try {
+                PUBLISHER.publish(m.getValue(), m.getTopic());
+            } catch (ServerPublisherBrokerException e) {
+                LOGGER.log(Level.WARNING, e);
+            }
+        }
+        LOGGER.log(Level.INFO, loggingBody + " User successfully deleted.");
         return Response.ok().build();
     }
     
