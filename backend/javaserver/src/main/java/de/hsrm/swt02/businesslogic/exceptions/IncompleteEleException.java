babb3a17fe28810d6bee86a855f2552cb21bdea5
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-12 10:01:38 +0000

    #39 add incompleteEleException

---
+++ backend/javaserver/src/main/java/de/hsrm/swt02/businesslogic/exceptions/IncompleteEleException.java
@@ -0,0 +1,35 @@
+package de.hsrm.swt02.businesslogic.exceptions;
+
+/**
+ * Exception for incomplete Elements.
+ *
+ */
+public class IncompleteEleException extends LogicException {
+    
+    private static final long serialVersionUID = -2169286945881690968L;
+    public static final int ERRORCODE = 11400;
+    
+    /**
+     * Constructor for the Exception.
+     */
+    public IncompleteEleException() {
+        super("item is incomplete");
+    }
+
+    /**
+     * Method dealing with the error message.
+     * @param msg is the errormessage.
+     */
+    public IncompleteEleException(String msg) {
+        super(msg);
+    }
+    
+    /**
+     * Getter for the errorcode.
+     * @return ERRORCODE is the errorcode
+     */
+    public int getErrorCode() {
+        return IncompleteEleException.ERRORCODE;
+    }
+
+}
