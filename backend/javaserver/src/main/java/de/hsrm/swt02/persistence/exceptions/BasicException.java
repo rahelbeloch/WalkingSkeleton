716ba7d9575b2d378544c4d188d6dad7c3fdead2
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-15 09:22:48 +0000

    #51 -- resolved exception dependencies

---
+++ backend/javaserver/src/main/java/de/hsrm/swt02/persistence/exceptions/BasicException.java
@@ -0,0 +1,23 @@
+package de.hsrm.swt02.persistence.exceptions;
+
+public class BasicException extends Exception {
+    
+    /**
+     * 
+     */
+    private static final long serialVersionUID = -1725732891907643731L;
+    private int errorcode = 10000;
+    
+    public BasicException() {
+        super();
+    }
+
+    public BasicException(String msg) {
+        super(msg);
+    }
+    
+    public int getErrorCode() {
+        return this.errorcode;
+    }
+
+}
