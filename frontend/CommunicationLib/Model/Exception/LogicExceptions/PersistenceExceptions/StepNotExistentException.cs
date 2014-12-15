ffa1215459737948c34c221a5bc6b47faded61f9
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-15 15:16:35 +0000

    #40 added stepnotexistentexception

---
+++ frontend/CommunicationLib/Model/Exception/LogicExceptions/PersistenceExceptions/StepNotExistentException.cs
@@ -0,0 +1,44 @@
+﻿using CommunicationLib.Exception;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Model.Exception.LogicExceptions.PersistenceExceptions
+{
+    class StepNotExistentException : DoesntExistsException
+    {
+         /// <summary>
+        /// This Exception es a child of DoesntExistsException. 
+        /// Each Exception has an number, here it is 11254. 
+        /// </summary>
+        private int _number = 11254;
+        new public int number { get { return _number; } }
+
+        public StepNotExistentException()
+            : base("Der Step existiert nicht.")
+        {
+
+        }
+
+        /// <summary>
+        /// This constructor allows to add a spezial message.
+        /// </summary>
+        /// <param name="message">the message</param>
+        public StepNotExistentException(string message)
+            : base(message)
+        {
+        }
+
+        /// <summary>
+        /// This constructor allows to add a spezial message and an other exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public StepNotExistentException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
