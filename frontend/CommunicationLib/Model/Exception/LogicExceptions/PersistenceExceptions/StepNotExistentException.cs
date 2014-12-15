827689a62682cfbb37a43029869f76e591337784
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-15 15:22:16 +0000

    #40 added stepnotexistentexception to staticresource

--- frontend/CommunicationLib/Model/Exception/LogicExceptions/PersistenceExceptions/StepNotExistentException.cs
+++ frontend/CommunicationLib/Model/Exception/LogicExceptions/PersistenceExceptions/StepNotExistentException.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CommunicationLib.Model.Exception.LogicExceptions.PersistenceExceptions
+namespace CommunicationLib.Exception
 {
     class StepNotExistentException : DoesntExistsException
     {
