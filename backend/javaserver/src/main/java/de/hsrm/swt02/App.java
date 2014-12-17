3145a68c3d55a47b1bc7c14a8e12ee44e30542b9
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-17 09:41:35 +0000

    #31 test comit for keyEvent testing

--- backend/javaserver/src/main/java/de/hsrm/swt02/App.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/App.java
@@ -1,5 +1,10 @@
 package de.hsrm.swt02;
 
+import java.awt.KeyEventDispatcher;
+import java.awt.KeyboardFocusManager;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import de.hsrm.swt02.constructionfactory.ConstructionFactory;
 import de.hsrm.swt02.logging.LogConfigurator;
 import de.hsrm.swt02.restserver.RestServer;
@@ -10,6 +15,9 @@ import de.hsrm.swt02.restserver.RestServer;
  * Starts the Rest-server.
  */
 public class App {
+    
+    private static RestServer server;
+    
     /**
      * Application startup method.
      * Configures logging.
@@ -18,7 +26,7 @@ public class App {
      * @param args are the program start parameters
      */
     public static void main(String[] args) {
-        final RestServer server;
+
         
         // setup log configuration
         LogConfigurator.setup();
@@ -37,8 +45,23 @@ public class App {
         Runtime.getRuntime().addShutdownHook(new Thread() {
             public void run() {
                 if (server != null) {
+                    System.err.println("ausfuehrung");
                     server.stopHTTPServer(true);
                 }
+            }
+        });
+        
+        //KeyEvents
+        KeyboardFocusManager.getCurrentKeyboardFocusManager().addKeyEventDispatcher(new KeyEventDispatcher() {  
+            @Override
+            public boolean dispatchKeyEvent(KeyEvent keyEvent) {
+                System.err.println("Bin da");
+                if (keyEvent.getKeyChar() == 'c' && keyEvent.isControlDown()) {
+                    server.stopHTTPServer(true);
+                    System.err.println("TOLL");
+                    return true;
+                } 
+                return false;
             }
         });
         
