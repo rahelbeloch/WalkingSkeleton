b0c2e68b0eaa80990b4d036dd21d13a95c217206
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-17 14:47:26 +0000

    #24 did rest testing

--- frontend/CommunicationLib/REST/RestAPI.cs
+++ frontend/CommunicationLib/REST/RestAPI.cs
@@ -106,6 +106,7 @@ namespace RestAPI
             // if no HttpException happened and although the StatusCode is not "OK", there must be on Exception of our own
             if (response.StatusCode != HttpStatusCode.OK && response.StatusCode == HttpStatusCode.InternalServerError)
             {
+                System.Diagnostics.Trace.WriteLine("!!!!!! " + response.Content);
                 int errorCode = Int32.Parse(response.Content);
 
                 //generate convenient exception
