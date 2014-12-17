77f67241dec8e38973fb410e6e3fadaa1f4e2a6f
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-17 11:44:53 +0000

    #61 cleaning messy code

--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/ConnectionException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/ConnectionException.java
@@ -2,24 +2,35 @@ package de.hsrm.swt02.restserver.exceptions;
 
 import de.hsrm.swt02.persistence.exceptions.BasicException;
 
+/**
+ * Exception for ConnectionExceptionhandling.
+ */
 public class ConnectionException extends BasicException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 1614570197373745612L;
-    private int errorcode = 12200;
+    public static final int ERRORCODE = 12200;
     
+    /**
+     * Constructor.
+     */
     public ConnectionException() {
         super();
     }
 
+    /**
+     * Method dealing with the exception message.
+     * @param msg the exception message
+     */
     public ConnectionException(String msg) {
         super(msg);
     }
     
+    /**
+     * Getter for errorcode.
+     * @return errorcode is the errorcode
+     */
     public int getErrorCode() {
-        return this.errorcode;
+        return ConnectionException.ERRORCODE;
     }
     
 }
--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/JacksonException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/JacksonException.java
@@ -1,23 +1,34 @@
 package de.hsrm.swt02.restserver.exceptions;
 
+/**
+ * Exception for JacksonExceptionhandling.
+ */
 public class JacksonException extends RestserverException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 5071158186141166613L;
-    private int errorcode = 12210;
+    public static final int ERRORCODE = 12210;
     
+    /**
+     * Constructor.
+     */
     public JacksonException() {
         super("Jackson parsing-error occured");
     }
 
+    /**
+     * Method dealing with the exception message.
+     * @param msg is the exception message
+     */
     public JacksonException(String msg) {
         super(msg);
     }
     
+    /**
+     * getter for the errorcode.
+     * @return errorcode is the errorcode
+     */
     public int getErrorCode() {
-        return this.errorcode;
+        return JacksonException.ERRORCODE;
     }
     
 }
