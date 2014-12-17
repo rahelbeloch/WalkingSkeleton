69e9cff93c62ffb0226a808dc8ab6afd038dd98d
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-17 11:44:53 +0000

    #61 cleaning messy code

--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/RestserverException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/exceptions/RestserverException.java
@@ -1,23 +1,34 @@
 package de.hsrm.swt02.restserver.exceptions;
 
+/**
+ * Exception for RestServerExceptionhandling.
+ */
 public class RestserverException extends ConnectionException {
 
-    /**
-     * 
-     */
     private static final long serialVersionUID = 7591430845597579348L;
-    private int errorcode = 12200;
+    public static final int ERRORCODE = 12200;
     
+    /**
+     * Constructor.
+     */
     public RestserverException() {
         super();
     }
 
+    /**
+     * Method dealing the exception message.
+     * @param msg exception message
+     */
     public RestserverException(String msg) {
         super(msg);
     }
     
+    /**
+     * Getter for error code.
+     * @return errorcode is the error code
+     */
     public int getErrorCode() {
-        return this.errorcode;
+        return RestserverException.ERRORCODE;
     }
     
 }
