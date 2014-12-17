f1d5e8fe64b2d3d1bf103dc8d4e78b8db1fcce60
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2014-12-17 11:48:49 +0000

    #61 cleaned messy code

--- backend/javaserver/src/main/java/de/hsrm/swt02/persistence/exceptions/BasicException.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/persistence/exceptions/BasicException.java
@@ -1,23 +1,34 @@
 package de.hsrm.swt02.persistence.exceptions;
 
+/**
+ * Exception for BasicExceptionhandling.
+ */
 public class BasicException extends Exception {
     
-    /**
-     * 
-     */
     private static final long serialVersionUID = -1725732891907643731L;
-    private int errorcode = 10000;
+    public static final int ERRORCODE = 10000;
     
+    /**
+     * Constructor.
+     */
     public BasicException() {
         super();
     }
 
+    /**
+     * Method dealing with the exception message.
+     * @param msg exception message
+     */
     public BasicException(String msg) {
         super(msg);
     }
     
+    /**
+     * Getter for the errorcode.
+     * @return errorcode is the errorcode
+     */
     public int getErrorCode() {
-        return this.errorcode;
+        return BasicException.ERRORCODE;
     }
 
 }
