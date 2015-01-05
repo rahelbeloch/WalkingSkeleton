5de762e43d6aa33f913325a89e611cbc6d45f384
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-05 10:44:27 +0000

    #84 changed header user authentification

--- frontend/CommunicationLib/REST/URLRouter.cs
+++ frontend/CommunicationLib/REST/URLRouter.cs
@@ -58,17 +58,6 @@ namespace CommunicationLib
 
                 String typeName = objType.FullName.Split('.').Last().ToLower();
                 url += "/" + typeName + "s";
-                //url += "/" + typeName + (url.Equals("command") ? "" : "s");
-                /*
-                if (url.Equals("command"))
-                {
-                    url += "/" + typeName;
-                }
-                else
-                {
-                    url += "/" + typeName + "s";
-                }
-                 */
             }
 
             // append all url params to url; seperated by '/'
