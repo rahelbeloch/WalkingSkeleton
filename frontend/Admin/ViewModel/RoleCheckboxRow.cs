f20d5ed4eed5715c867a6583a9972cce30d9d1c8
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-05 16:28:04 +0000

    #54 added comments

--- frontend/Admin/ViewModel/RoleCheckboxRow.cs
+++ frontend/Admin/ViewModel/RoleCheckboxRow.cs
@@ -7,11 +7,20 @@ using System.Threading.Tasks;
 
 namespace Admin.ViewModel
 {
+    /// <summary>
+    /// This class is used for as a DataBinding for a row in a ListView Control.
+    /// </summary>
     public class RoleCheckboxRow : ViewModelBase
     {
+        /// <summary>
+        /// Property to bind the role to a row.
+        /// </summary>
         private Role _role;
         public Role Role { get { return _role; } set { _role = value; } }
 
+        /// <summary>
+        /// Property to (de)select the checkbox in a row.
+        /// </summary>
         private Boolean _isSelected;
         public Boolean IsSelected 
         { 
