e251fa1761c4cbbc8811240b94e6d6dc925f5aa0
eschn002 <eschn002@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-07 16:17:35 +0000

    #90 add From

---
+++ frontend/CommunicationLib/Model/DataModel/Form.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Model.DataModel
+{
+    class Form : RootElement
+    {
+        private Dictionary<string, string> _formDef;
+        public Dictionary<string, string> formDef { get { return _formDef; } }
+
+        private string _description;
+        public string description { get { return _description; } set { _description = value; } }
+
+        public Form()
+            : base()
+        {
+            this._formDef = new Dictionary<string, string>();
+            this._description = "";
+        }
+        public Form(string description)
+            : base()
+        {
+            this._formDef = new Dictionary<string, string>();
+            this._description = description;
+        }
+    }
+}
