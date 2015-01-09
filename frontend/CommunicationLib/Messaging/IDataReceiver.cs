d1c53638f97714dfe13b23ad636a741d37051e53
jfick001 <jfick001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-09 10:41:09 +0000

    #100 updated IDataReceiver for delete information.

--- frontend/CommunicationLib/Messaging/IDataReceiver.cs
+++ frontend/CommunicationLib/Messaging/IDataReceiver.cs
@@ -9,11 +9,54 @@ namespace CommunicationLib
     /// </summary>
     public interface IDataReceiver
     {
+        /// <summary>
+        /// Callback method for source updates.
+        /// This method is called by the Communication manager for workflow updates.
+        /// </summary>
+        /// <param name="updatedWorkflow">the updated workflow source</param>
         void WorkflowUpdate(Workflow updatedWorkflow);
+
+        /// <summary>
+        /// Callback method for source updates.
+        /// This method is called by the Communication manager for item updates.
+        /// </summary>
+        /// <param name="updatedWorkflow">the updated item source</param>
         void ItemUpdate(Item updatedItem);
+
+        /// <summary>
+        /// Callback method for source updates.
+        /// This method is called by the Communication manager for user updates.
+        /// </summary>
+        /// <param name="updatedWorkflow">the updated user source</param>
         void UserUpdate(User updatedUser);
+
+        /// <summary>
+        /// Callback method for source updates.
+        /// This method is called by the Communication manager for role updates.
+        /// </summary>
+        /// <param name="updatedWorkflow">the updated role source</param>
         void RoleUpdate(Role updatedRole);
+
+        /// <summary>
+        /// Callback method for source updates.
+        /// This method is called by the CommunicationManager for form updates.
+        /// </summary>
+        /// <param name="updatedWorkflow">the updated form source</param>
         void FormUpdate(Form updatedForm);
+
+        /// <summary>
+        /// Callback method for source deletion updates.
+        /// This method is called by the CommunicationManager for source deletions.
+        /// </summary>
+        /// <param name="sourceType">type of the deleted source</param>
+        /// <param name="sourceId">identifier of the deleted source</param>
+        void DataDeletion(Type sourceType, string sourceId);
+
+        /// <summary>
+        /// Callback method for exception handling.
+        /// This will be called for all exception occurrences within the comLib.
+        /// </summary>
+        /// <param name="e">occurred exception</param>
         void HandleError(System.Exception e);
     }
 }
