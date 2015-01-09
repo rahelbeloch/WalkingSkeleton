7d68951262f2448198ec6a436d10079319f28f8d
akoen001 <akoen001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-09 09:37:28 +0000

    #93 added getAllForms test

--- backend/javaserver/src/test/java/de/hsrm/testswt02/RestserverTesting/RestserverFormTest.java
+++ backend/javaserver/src/test/java/de/hsrm/testswt02/RestserverTesting/RestserverFormTest.java
@@ -1,11 +1,12 @@
 package de.hsrm.testswt02.RestserverTesting;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.List;
 import java.util.Properties;
 
 import javax.ws.rs.client.Client;
@@ -20,9 +21,11 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.hsrm.swt02.logging.LogConfigurator;
+import de.hsrm.swt02.model.User;
 import de.hsrm.swt02.restserver.RestServer;
 
 /**
@@ -97,6 +100,54 @@ public class RestserverFormTest {
                 .post(Entity.entity(dataform,
                         MediaType.APPLICATION_FORM_URLENCODED));
         assertEquals(httpstatus, resp.getStatus());
+    }
+    
+    /**
+     * This test checks if the client can get a new user successfully.
+     * It succeeds if the requested user is the same as the sent user.
+     */
+    @Test
+    public void testGetAllForms() {
+        final ObjectMapper mapper = new ObjectMapper();
+        String formsAsString = null;
+        List<de.hsrm.swt02.model.Form> testForms = null;
+        
+        final de.hsrm.swt02.model.Form testForm = new de.hsrm.swt02.model.Form();
+        final Form dataform;
+        String formAsString = null;
+        testForm.setDescription("Ein weiteres Form.");
+        
+        try {
+            formAsString = mapper.writeValueAsString(testForm);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        dataform = new Form().param("data", formAsString);
+        client
+                .target(targetUrl)
+                .path("resource/forms")
+                .request()
+                .header("username", headerUsername)
+                .header("password", headerPW)
+                .header("clientID",headerClientID)
+                .post(Entity.entity(dataform,
+                        MediaType.APPLICATION_FORM_URLENCODED));
+        
+        formsAsString = client.target(targetUrl)
+                .path("resource/forms").request()
+                .header("username", headerUsername)
+                .header("password", headerPW)
+                .header("clientID",headerClientID)
+                .get(String.class);
+        
+        try {
+            testForms = mapper.readValue(formsAsString, new TypeReference<List<de.hsrm.swt02.model.Form>>() { } );
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        
+        assertTrue(testForms.contains(testForm));
+
     }
     
     /**
