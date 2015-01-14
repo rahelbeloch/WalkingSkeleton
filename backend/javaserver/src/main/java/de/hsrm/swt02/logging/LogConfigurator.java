7c872d32379ba4ca1b6354fe5cef46adee9456e1
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-14 12:14:26 +0000

    #103 fixed logConfigurator.class property loading.

--- backend/javaserver/src/main/java/de/hsrm/swt02/logging/LogConfigurator.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/logging/LogConfigurator.java
@@ -1,8 +1,5 @@
 package de.hsrm.swt02.logging;
 
-import java.io.BufferedInputStream;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
@@ -14,6 +11,8 @@ import java.util.logging.Formatter;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import de.hsrm.swt02.properties.ConfigProperties;
 
 /**
  * Class for setting up the global logger. Invoke setup()-Method to set the
@@ -40,31 +39,14 @@ public class LogConfigurator {
     }
     
     /**
-     * Setup method to load the logger configuration from the 'server.config'
-     * file. Keep in mind that the configuration is a global setting for the
+     * Setup method to load the logger configuration from the ConfigProperties.class.
+     * Keep in mind that the configuration is a global setting for the
      * UseLogger.class. That means you have to call this method just once to
      * setup your configuration.
-     * 
-     * @throws IOException if server.config file can't be found
      */
     public static void setup() {
-        final Logger logger = Logger.getLogger(UseLogger.class.getName());
-        
-        final Properties properties = new Properties();
-        BufferedInputStream stream;
-        // read configuration file for rest properties
-        try {
-            stream = new BufferedInputStream(new FileInputStream(
-                    "server.config"));
-            properties.load(stream);
-            stream.close();
-        } catch (FileNotFoundException e) {
-            logger.log(Level.SEVERE, "Configuration file not found!");
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Can't read file!");
-        } catch (SecurityException e) {
-            logger.log(Level.SEVERE, "Read Access not granted!");
-        }
+        final Logger logger = Logger.getLogger(UseLogger.class.getName()); 
+        final Properties properties = ConfigProperties.getInstance().getProperties();
         
         // properties
         final Level logLevel = levelMap.get(properties.getProperty("LogLevel"));
@@ -101,51 +83,4 @@ public class LogConfigurator {
         }
     }
     
-    
-    /**
-     * Setup method to load the logger configuration from the 'server.config'
-     * file. Keep in mind that the configuration is a global setting for the
-     * UseLogger.class. That means you have to call this method just once to
-     * setup your configuration.
-     * 
-     * @throws IOException if server.config file can't be found
-     * @param properties that have been read out of the config file
-     */
-    public static void setup(Properties properties) {
-        final Logger logger = Logger.getLogger(UseLogger.class.getName());
-        
-        // properties
-        final Level logLevel = levelMap.get(properties.getProperty("LogLevel"));
-        final String consoleLogging = properties.getProperty("ConsoleLogging");
-        final String logFile = properties.getProperty("LogFile");
-
-        // default level for logging
-        logger.setLevel(logLevel);
-        // set handler for a specific log file
-        try {
-            fileHTMLHandler = new FileHandler(logFile, LOGFILE_SIZE, LOGFILE_AMOUNT);
-        } catch (SecurityException e) {
-            logger.log(Level.WARNING,
-                    "Couldn't create a file handler for the logging file.");
-        } catch (IOException e) {
-            logger.log(Level.WARNING,
-                    "Couldn't create a file handler for the logging file.");
-        }
-        // set HTML-Formatter for a logging handler
-        formatterHTML = new HTMLFormatter();
-        fileHTMLHandler.setFormatter(formatterHTML);
-        logger.addHandler(fileHTMLHandler);
-
-        // disable console logging and remove the default
-        final Logger rootLogger = Logger.getLogger("");
-        final Handler[] handlers = rootLogger.getHandlers();
-        if (handlers[0] instanceof ConsoleHandler) {
-            rootLogger.removeHandler(handlers[0]);
-        }
-        // add consoleHandler if necessary
-        if (consoleLogging.equals("true")) {
-            final ConsoleLogHandler ourConsoleHandler = new ConsoleLogHandler();
-            rootLogger.addHandler(ourConsoleHandler);
-        }
-    }
 }
