80222dda66079d01e1af21fbfffc55ba4aee920e
sehre001 <sehre001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-14 12:12:27 +0000

    #103 server config adminitsration is now centralized in the ConfigProperties.class

--- backend/javaserver/src/main/java/de/hsrm/swt02/properties/ConfigProperties.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/properties/ConfigProperties.java
@@ -1,49 +1,67 @@
 package de.hsrm.swt02.properties;
 
 import java.io.BufferedInputStream;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.Properties;
 import java.util.logging.Level;
 
 import de.hsrm.swt02.logging.UseLogger;
 
 /**
- * 
  * Class responsible for loading the properties initially. can be globally accessed to get Properties. Implements
  * singleton pattern.
- * 
- * @author akoen001
  *
  */
 public class ConfigProperties {
     
+    private static final String CONFIG_NAME = "server.config";
+    
     private static ConfigProperties theInstance;
     private Properties properties;
-    private static UseLogger logger;
+    private UseLogger logger;
     
     /**
-     * 
-     * constructor.
-     * 
+     * private constructor.
      */
     private ConfigProperties() {
         logger = new UseLogger();
-        properties = new Properties();
         BufferedInputStream stream;
-        // read configuration file for rest properties
-        try {
-            stream = new BufferedInputStream(new FileInputStream(
-                        "server.config"));
-            properties.load(stream);
-            stream.close();
-        } catch (FileNotFoundException e) {
-            logger.log(Level.SEVERE, "Configuration file not found!");
-        } catch (IOException e) {
-            logger.log(Level.SEVERE, "Can't read file!");
-        } catch (SecurityException e) {
-            logger.log(Level.SEVERE, "Read Access not granted!");
+        properties = new Properties();
+        
+        if (findExistingConfig()) {
+            try {
+             // read configuration file for rest properties
+                stream = new BufferedInputStream(new FileInputStream( CONFIG_NAME ));
+                properties.load(stream);
+                stream.close();
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Can't read file!");
+            }
+        }
+        else {
+            try {
+                // write default configuration file
+                final PrintWriter pWriter = new PrintWriter(new BufferedWriter(new FileWriter( CONFIG_NAME )));
+                pWriter.println("BrokerURL = vm://localhost");
+                pWriter.println("BrokerConnectionURL = tcp://0.0.0.0:61616");
+                pWriter.println("RestServerURI = http://0.0.0.0:18887/");
+                pWriter.println("LogFile = serverlog.html");
+                pWriter.println("LogLevel = info");
+                pWriter.println("ConsoleLogging = true");
+                pWriter.flush();
+                pWriter.close();
+                // and read it
+                stream = new BufferedInputStream(new FileInputStream( CONFIG_NAME ));
+                properties.load(stream);
+                stream.close();
+            } catch (IOException e) {
+                logger.log(Level.WARNING, "Configuration file could not be generated!");
+            }
         }
     }
     
@@ -60,6 +78,26 @@ public class ConfigProperties {
         return theInstance;
     }
    
+    /**
+     * Method to determine whether or not a config file is already existing.
+     * @return true if the config file is existing, false if not
+     */
+    private static boolean findExistingConfig() {
+        
+        final File dir = new File(".");
+        final String configName = ("server.config");
+        
+        final File[] files = dir.listFiles();
+        if (files != null) {
+            for (int i = 0; i < files.length; i++) {
+                if (files[i].getName().equalsIgnoreCase(configName)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
     /**
      * 
      * getter for the properties.
