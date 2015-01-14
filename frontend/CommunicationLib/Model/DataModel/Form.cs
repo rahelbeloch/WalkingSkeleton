31a7d9e2e7e4bde5792f3132326cfbe8d7b1face
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-14 17:22:10 +0000

    #90 changed data model of form (now list of formentries instead of map)

--- frontend/CommunicationLib/Model/DataModel/Form.cs
+++ frontend/CommunicationLib/Model/DataModel/Form.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CommunicationLib.Model.DataModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,24 +9,23 @@ namespace CommunicationLib.Model
 {
     public class Form : RootElement
     {
-        private Dictionary<string, string> _formDef;
-        public Dictionary<string, string> formDef { get { return _formDef; } set { _formDef = value; } }
+        private List<FormEntry> _formDef;
+        public List<FormEntry> formDef { get { return _formDef; } set { _formDef = value; } }
+
         private string _description;
         public string description { get { return _description; } set { _description = value; } }
 
         public Form()
             : base()
         {
-            _formDef = new Dictionary<string, string>();
-            _formDef.Add("key", "value");
+            _formDef = new List<FormEntry>();
             _description = "";
         }
 
         public Form(string description)
             : base()
         {
-            _formDef = new Dictionary<string, string>();
-            _formDef.Add("key", "value");
+            _formDef = new List<FormEntry>();
             _description = description;
         }
     }
