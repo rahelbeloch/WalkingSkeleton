ee31a3c5d12ebc8a044f4ca3fb20027f595d7788
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-15 16:12:47 +0000

    #91 form definitions can be removed now

--- frontend/CommunicationLib/Model/DataModel/FormEntry.cs
+++ frontend/CommunicationLib/Model/DataModel/FormEntry.cs
@@ -20,10 +20,19 @@ namespace CommunicationLib.Model.DataModel
         private string _value;
         public string value { get { return _value; } set { _value = value; } }
 
-        
+        /// <summary>
+        /// Used for client view. Will not be (de)serialized.
+        /// </summary>
         private List<String> _datatypes;
         [JsonIgnore]
         public List<String> datatypes { get { return _datatypes; } set { _datatypes = value; } }
+
+        /// <summary>
+        /// Used for client view. Will not be (de)serialized.
+        /// </summary>
+        private bool _selected;
+        [JsonIgnore]
+        public bool selected { get { return _selected; } set { _selected = value; } }
 
         public FormEntry(): base() 
         {
@@ -31,6 +40,7 @@ namespace CommunicationLib.Model.DataModel
             _datatypes.Add("String");
             _datatypes.Add("Int");
             _datatypes.Add("Float");
+            _selected = false;
         }
 
     }
