65c42c362de0ffae5083dd0d1fc278aeb7b0fdcf
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-16 13:08:02 +0000

    #90 removed property selected

--- frontend/CommunicationLib/Model/DataModel/FormEntry.cs
+++ frontend/CommunicationLib/Model/DataModel/FormEntry.cs
@@ -27,20 +27,12 @@ namespace CommunicationLib.Model.DataModel
         [JsonIgnore]
         public List<String> datatypes { get { return _datatypes; } set { _datatypes = value; } }
 
-        /// <summary>
-        /// Used for client view. Will not be (de)serialized.
-        /// </summary>
-        private bool _selected;
-        [JsonIgnore]
-        public bool selected { get { return _selected; } set { _selected = value; } }
-
         public FormEntry(): base() 
         {
             _datatypes = new List<String>();
             _datatypes.Add("String");
             _datatypes.Add("Int");
             _datatypes.Add("Float");
-            _selected = false;
         }
 
     }
