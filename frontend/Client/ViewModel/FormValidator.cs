258f9ccaf951c421351bca658390e294596ff8d0
khils001 <khils001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-19 11:57:42 +0000

    #96 implemented validator class for the Formular

---
+++ frontend/Client/ViewModel/FormValidator.cs
@@ -0,0 +1,33 @@
+﻿using NLog;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Client.ViewModel
+{
+    public class FormValidator : ValidationRule
+    {
+        private readonly Logger logger = LogManager.GetCurrentClassLogger();
+        private Type _type;
+        public Type type { get { return _type; } set { _type = value; } }
+        public override ValidationResult Validate
+          (object value, System.Globalization.CultureInfo cultureInfo)
+        {
+            logger.Debug(type.ToString);
+            StringConverter stringConverter = new StringConverter();
+            try
+            {
+                stringConverter.ConvertTo(value, type);
+            }
+            catch (Exception e)
+            {
+                return new ValidationResult(false, value.ToString()+" ist nicht vom Typ "+_type.ToString());
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
