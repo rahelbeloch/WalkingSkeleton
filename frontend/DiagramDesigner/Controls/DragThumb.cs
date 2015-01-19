0d86537ec9e9837e2b14c1459537984a50e9ae46
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-19 16:58:39 +0000

    #105 locked canvas when a workflow is selected

--- frontend/DiagramDesigner/Controls/DragThumb.cs
+++ frontend/DiagramDesigner/Controls/DragThumb.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Controls.Primitives;
 using System.Windows.Media;
@@ -29,16 +30,22 @@ namespace DiagramDesigner.Controls
 
                 foreach (DesignerItemViewModelBase item in designerItems.OfType<DesignerItemViewModelBase>())
                 {
-                    double left = item.Left;
-                    double top = item.Top;
-                    minLeft = double.IsNaN(left) ? 0 : Math.Min(left, minLeft);
-                    minTop = double.IsNaN(top) ? 0 : Math.Min(top, minTop);
+                    if (!((DiagramViewModel)item.Parent).locked)
+                    {
+                        double left = item.Left;
+                        double top = item.Top;
+                        minLeft = double.IsNaN(left) ? 0 : Math.Min(left, minLeft);
+                        minTop = double.IsNaN(top) ? 0 : Math.Min(top, minTop);
 
-                    double deltaHorizontal = Math.Max(-minLeft, e.HorizontalChange);
-                    double deltaVertical = Math.Max(-minTop, e.VerticalChange);
-                    item.Left += deltaHorizontal;
-                    item.Top += deltaVertical;
-
+                        double deltaHorizontal = Math.Max(-minLeft, e.HorizontalChange);
+                        double deltaVertical = Math.Max(-minTop, e.VerticalChange);
+                        item.Left += deltaHorizontal;
+                        item.Top += deltaVertical;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Sie können den Workflow nicht mehr ändern. Klicken Sie rechts auf Bearbeiten.");
+                    }
                 }
                 e.Handled = true;
             }
