c80c8f8e2b2b08c2100f7f9b8172d3de6d62d383
dneux001 <dneux001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-19 12:03:38 +0000

    #104 -- modified initExtension due to new validation Method

--- backend/javaserver/src/test/java/de/hsrm/testswt02/logic_unittest/LogicTest.java
+++ backend/javaserver/src/test/java/de/hsrm/testswt02/logic_unittest/LogicTest.java
@@ -58,7 +58,7 @@ public class LogicTest {
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test
     public void addGetWorkflowTest() throws LogicException {
@@ -69,7 +69,7 @@ public class LogicTest {
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test
     public void startWorkflowTest() throws LogicException {
@@ -91,27 +91,27 @@ public class LogicTest {
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test(expected = WorkflowNotExistentException.class)
     public void deleteWortflowTest() throws LogicException {
         init();
         li.addWorkflow(w);
         li.deleteWorkflow(w.getId());
-        
+
         li.getWorkflow(w.getId());
 
     }
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test
     public void stepOverTest() throws LogicException {
         init();
-        initExtension();        
-        
+        initExtension();
+
         Workflow testwf = new Workflow();
         Role r = new Role();
         StartStep ss = new StartStep();
@@ -131,33 +131,32 @@ public class LogicTest {
         testwf.addStep(fs);
         u.setUsername("user");
         u.addRole(r);
-        
+
         li.addRole(r);
         li.addUser(u);
         li.addWorkflow(testwf);
-        
+
         li.startWorkflow(testwf.getId(), u.getUsername());
-        
+
         final Workflow workflow = li.getWorkflow(testwf.getId());
         Item item = workflow.getItemByPos(0);
         System.out.println(item.getId());
         Item item2 = li.getItem(item.getId(), u.getUsername());
         System.out.println(item2.getId());
-        
-        li.stepForward(item.getId(), workflow.getStepById(a.getId())
-                .getId(), u.getUsername());
-        
-        li.stepForward(item.getId(), workflow.getStepById(a.getId())
-                .getId(), u.getUsername());
 
-        
+        li.stepForward(item.getId(), workflow.getStepById(a.getId()).getId(),
+                u.getUsername());
+
+        li.stepForward(item.getId(), workflow.getStepById(a.getId()).getId(),
+                u.getUsername());
+
         item = li.getItem(item.getId(), u.getUsername());
         assertTrue(item.getStepState(a.getId()) == "DONE");
     }
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test
     public void addStepTest() throws LogicException {
@@ -172,10 +171,9 @@ public class LogicTest {
 
     }
 
-  
     /**
      * 
-     * @throws PersistenceException 
+     * @throws PersistenceException
      */
     @Test
     public void addGetUserTest() throws PersistenceException {
@@ -185,13 +183,13 @@ public class LogicTest {
         } catch (UserAlreadyExistsException e) {
             assertTrue(false);
         }
-        
+
         assertTrue(li.getUser(user.getUsername()).equals(user));
     }
 
     /**
      * 
-     * @throws PersistenceException 
+     * @throws PersistenceException
      */
     @Test(expected = UserNotExistentException.class)
     public void deleteUserTest() throws PersistenceException {
@@ -208,12 +206,14 @@ public class LogicTest {
     /**
      * 
      * @throws LogicException if something went wrong in logic
-     * @throws UserNotExistentException 
-     * @throws WorkflowNotExistentException 
-     * @throws StorageFailedException 
+     * @throws UserNotExistentException
+     * @throws WorkflowNotExistentException
+     * @throws StorageFailedException
      */
     @Test
-    public void getWorkflowsByUser() throws LogicException, WorkflowNotExistentException, UserNotExistentException, StorageFailedException {
+    public void getWorkflowsByUser() throws LogicException,
+            WorkflowNotExistentException, UserNotExistentException,
+            StorageFailedException {
         init();
         initExtension();
 
@@ -224,31 +224,33 @@ public class LogicTest {
 
     /**
      * 
-     * @throws LogicException 
+     * @throws LogicException
      */
     @Test
     public void getOpenItemsByUserTest() throws LogicException {
         init();
         initExtension();
-        
+
         li.startWorkflow(w.getId(), user.getUsername());
         li.startWorkflow(w.getId(), user.getUsername());
 
-        final int i = li.getRelevantItemsForUser(w.getId(), user.getUsername()).size();
+        final int i = li.getRelevantItemsForUser(w.getId(), user.getUsername())
+                .size();
         assertTrue(i == 2);
 
     }
-    
+
     /**
      * Check if steps in a workflow are connected.
-     * @throws LogicException 
+     * 
+     * @throws LogicException
      */
     @Test
     public void connectWorkflowSteps() throws LogicException {
         init();
         initExtension();
-        
-        for (Step step: w2.getSteps()) {
+
+        for (Step step : w2.getSteps()) {
             if (!(step instanceof FinalStep)) {
                 assertTrue(step.getNextSteps().size() == 1);
             }
@@ -258,21 +260,25 @@ public class LogicTest {
     /**
      * 
      * @throws LogicException if something went wrong in logic.
-     * @throws WorkflowNotExistentException 
-     * @throws UserNotExistentException 
-     * @throws StorageFailedException 
+     * @throws WorkflowNotExistentException
+     * @throws UserNotExistentException
+     * @throws StorageFailedException
      */
     @Test
-    public void getStartableWorkflowsTest() throws LogicException, UserNotExistentException, WorkflowNotExistentException, StorageFailedException {
+    public void getStartableWorkflowsTest() throws LogicException,
+            UserNotExistentException, WorkflowNotExistentException,
+            StorageFailedException {
         init();
         initExtension();
-        final int i = li.getStartableWorkflowsForUser(user2.getUsername()).size();
+        final int i = li.getStartableWorkflowsForUser(user2.getUsername())
+                .size();
         assertTrue(i == 3);
     }
 
     /**
      * This Method test deactivation.
-     * @throws LogicException 
+     * 
+     * @throws LogicException
      */
     @Test
     public void deactivateWorkflow() throws LogicException {
@@ -281,54 +287,55 @@ public class LogicTest {
         w.setActive(false);
         assertFalse(w.isActive());
     }
-    
-    
+
     /**
      * 
      * @throws UserNotExistentException if user doesnt exisis.
      * @throws LogicException if something went wrong in logic.
-     * @throws WorkflowNotExistentException 
-     * @throws StorageFailedException 
+     * @throws WorkflowNotExistentException
+     * @throws StorageFailedException
      */
     @Test
-    public void getAllActiveWorkflowsByUserTest() throws UserNotExistentException, LogicException, WorkflowNotExistentException, StorageFailedException {
+    public void getAllActiveWorkflowsByUserTest()
+            throws UserNotExistentException, LogicException,
+            WorkflowNotExistentException, StorageFailedException {
         init();
-        initExtension();       
+        initExtension();
         final int before = li.getAllWorkflowsForUser(user.getUsername()).size();
-        
+
         li.deactivateWorkflow(w.getId());
-        
-         
+
         final int after = li.getAllWorkflowsForUser(user.getUsername()).size();
 
         assertTrue(before == after + 1);
-        
+
     }
+
     /**
      * open items should show even the workflow is deaktive.
+     * 
      * @throws LogicException .
      */
- 
-    
 
     /**
      * this method init basic data for testing.
-     * @throws StorageFailedException 
+     * 
+     * @throws StorageFailedException
      */
     private void init() throws StorageFailedException {
         final Injector i = Guice.createInjector(new SingleModule());
         li = i.getInstance(Logic.class);
-        
+
         role = new Role();
         role.setRolename("role");
-        
+
         user = new User();
         user.setUsername("0");
         user.getRoles().add(role);
-        
+
         final ArrayList<String> roles = new ArrayList<String>();
         roles.add(role.getRolename());
-        
+
         startStep = new StartStep(roles);
         action = new Action(roles, "description");
         finalStep1 = new FinalStep();
@@ -340,7 +347,7 @@ public class LogicTest {
         w.addStep(startStep);
         w.addStep(action);
         w.addStep(finalStep1);
-        
+
         try {
             li.addRole(role);
             li.addUser(user);
@@ -351,33 +358,33 @@ public class LogicTest {
             e.printStackTrace();
         }
     }
-    
+
     /**
      * This method init more data for testing.
-     * @throws LogicException 
-
+     * 
+     * @throws LogicException
      */
     private void initExtension() throws LogicException {
-        
+
         role1 = new Role();
         role1.setRolename("role1");
-        
+
         role2 = new Role();
         role2.setRolename("role2");
-        
+
         user1 = new User();
         user1.setUsername("1");
         user1.getRoles().add(role1);
-        
+
         user2 = new User();
         user2.setUsername("2");
         user2.getRoles().add(role2);
-        
+
         final ArrayList<String> roles1 = new ArrayList<String>();
         roles1.add(role1.getRolename());
         final ArrayList<String> roles2 = new ArrayList<String>();
         roles2.add(role2.getRolename());
-        
+
         startStep1 = new StartStep(roles1);
         startStep2 = new StartStep(roles2);
         startStep3 = new StartStep(roles2);
@@ -386,7 +393,7 @@ public class LogicTest {
         action3 = new Action(roles2, "description");
         finalStep1 = new FinalStep();
         finalStep2 = new FinalStep();
-        
+
         startStep1.getRoleIds().add(role1.getRolename());
         startStep2.getRoleIds().add(role1.getRolename());
         startStep3.getRoleIds().add(role1.getRolename());
@@ -395,40 +402,42 @@ public class LogicTest {
         action3.getRoleIds().add(role2.getRolename());
         finalStep1.getRoleIds().add(role2.getRolename());
         finalStep2.getRoleIds().add(role2.getRolename());
-        
+
         w1 = new Workflow();
         w1.addStep(startStep1);
         w1.addStep(action1);
         w1.addStep(finalStep1);
-        
+
         w2 = new Workflow();
         w2.addStep(startStep2);
         w2.addStep(action2);
         w2.addStep(finalStep2);
-        
+
         w3 = new Workflow();
         w3.addStep(startStep3);
         w3.addStep(action3);
         w3.addStep(finalStep2);
-        
-        try {
-            li.addWorkflow(w1);
-            li.addWorkflow(w2);
-            li.addWorkflow(w3);
-        
-        } catch (IncompleteEleException e1) {
-            e1.printStackTrace();
-        }
-        
+
         try {
             li.addRole(role1);
             li.addRole(role2);
             li.addUser(user2);
             li.addUser(user1);
-            
+
         } catch (UserAlreadyExistsException e) {
             assertTrue(false);
         }
+        
+        try {
+            li.addWorkflow(w1);
+            li.addWorkflow(w2);
+            li.addWorkflow(w3);
+
+        } catch (IncompleteEleException e1) {
+            e1.printStackTrace();
+        }
+
+
     }
 
 }
