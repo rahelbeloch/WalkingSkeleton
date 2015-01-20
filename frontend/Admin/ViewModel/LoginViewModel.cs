6d7978176b33d5db3cd2d1e0db345c8a1d579577
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-20 12:03:37 +0000

    #111 implemented the server and broker address textbox + binding + saving in config file

--- frontend/Admin/ViewModel/LoginViewModel.cs
+++ frontend/Admin/ViewModel/LoginViewModel.cs
@@ -9,6 +9,9 @@ using CommunicationLib.Exception;
 using System.Windows;
 using CommunicationLib;
 using NLog;
+using System.Configuration;
+using System.Reflection;
+using System.Text.RegularExpressions;
 
 namespace Admin.ViewModel
 {
@@ -24,6 +27,10 @@ namespace Admin.ViewModel
             : base()
         {
             _mainViewModel = mainViewModel;
+            _serverAddress = ConfigurationManager.AppSettings["ServerAddress"];
+            _brokerAddress = ConfigurationManager.AppSettings["BrokerAddress"];
+            Console.WriteLine(_serverAddress);
+            Console.WriteLine(_brokerAddress);
         }
 
         /// <summary>
@@ -42,6 +49,7 @@ namespace Admin.ViewModel
                 OnChanged("securePwd");
             }
         }
+
         /// <summary>
         /// Property for input from username text box.
         /// </summary>
@@ -58,6 +66,41 @@ namespace Admin.ViewModel
                 OnChanged("admin");
             }
         }
+
+        /// <summary>
+        /// Property for server adress.
+        /// </summary>
+        private string _serverAddress;
+        public string ServerAddress
+        {
+            get
+            {
+                return _serverAddress;
+            }
+            set
+            {
+                _serverAddress = value;
+                OnChanged("serverAddress");
+            }
+        }
+
+        /// <summary>
+        /// Property for broker adress.
+        /// </summary>
+        private string _brokerAddress;
+        public string BrokerAddress
+        {
+            get
+            {
+                return _brokerAddress;
+            }
+            set
+            {
+                _brokerAddress = value;
+                OnChanged("brokerAddress");
+            }
+        }
+
         /// <summary>
         /// ICommand which is called by the login button
         /// </summary>
@@ -73,6 +116,10 @@ namespace Admin.ViewModel
                     {
                         try
                         {
+                            // save server and broker address to config file (admin.exe)
+                            SaveAddresses();
+                            _mainViewModel.myComLib.RefreshUrls(_serverAddress, _brokerAddress);
+
                             // Register mainViewModel to CommunicationLib (if login worked)
                             _mainViewModel.myComLib.Login(admin, securePwd);
                             _mainViewModel.InitModel();
@@ -91,6 +138,58 @@ namespace Admin.ViewModel
                 }
                 return _authenticate;
             }
+        }
+
+        private void SaveAddresses()
+        {
+            string server = "http";
+            string broker = "tcp";
+
+            // retrieve local config file path from filesystem
+            string uriPath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
+            // save server and broker address to config file
+            Configuration config = ConfigurationManager.OpenExeConfiguration(uriPath);
+
+            if (config.AppSettings.Settings["ServerAddress"] == null && config.AppSettings.Settings["BrokerAddress"] == null)
+            {
+                config.AppSettings.Settings.Add("ServerAddress", _serverAddress);
+                config.AppSettings.Settings.Add("BrokerAddress", _brokerAddress);
+            }
+            else
+            {
+                config.AppSettings.Settings["ServerAddress"].Value = _serverAddress;
+                config.AppSettings.Settings["BrokerAddress"].Value = _brokerAddress;
+            }
+
+            //// test if given addresses are valid
+            //if (ValidateAddress(server, _serverAddress) && ValidateAddress(broker, _brokerAddress))
+            //{
+            //    config.AppSettings.Settings["ServerAddress"].Value = _serverAddress;
+            //    config.AppSettings.Settings["BrokerAddress"].Value = _brokerAddress;
+            //}
+            //else
+            //{
+            //    // TODO: Exception werfen, wenn regex nicht passt!
+            //    //MessageBox.Show("Server oder Broker Adresse syntaktisch nicht korrekt.");
+            //    config.AppSettings.Settings["ServerAddress"].Value = _serverAddress;
+            //    config.AppSettings.Settings["BrokerAddress"].Value = _brokerAddress;
+            //}
+
+            config.Save(ConfigurationSaveMode.Modified);
+            ConfigurationManager.RefreshSection("appSettings");
+        }
+
+        private bool ValidateAddress(string pre, string matchString)
+        {
+            string ipPattern = pre + ":\\\\\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b:[0-9]{4,}";
+            string localPattern =  pre + ":\\\\localhost:[0-9]{4,}";
+            
+            // calling regex for ip/local server address
+            Match matchIp = Regex.Match(matchString, ipPattern, RegexOptions.IgnoreCase);
+            Match matchLocal = Regex.Match(matchString, localPattern, RegexOptions.IgnoreCase);
+
+            // check the match success
+            return matchIp.Success || matchLocal.Success;
         }
     }
 }
