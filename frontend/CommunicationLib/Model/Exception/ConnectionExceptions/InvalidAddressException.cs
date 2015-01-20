ba8c6165a374b88e2afb1c440a6db3141f28a86e
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-20 12:35:25 +0000

    #111 implemented validation for server and broker address

---
+++ frontend/CommunicationLib/Model/Exception/ConnectionExceptions/InvalidAddressException.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Exception
+{
+    public class InvalidAddressException : ConnectionException
+    {
+        /// <summary>
+        /// This Exception es a child of BasicException. 
+        /// Each Exception has an number, here it is 12000. 
+        /// </summary>
+        private int _number = 12400;
+        new public int number { get { return _number; } }
+
+        public InvalidAddressException()
+            : base("Die Server- oder Broker Adresse ist syntaktisch nicht korrekt.")
+        {
+        }
+
+        /// <summary>
+        /// This constructor allows to add a spezial message.
+        /// </summary>
+        /// <param name="message">the message</param>
+        public InvalidAddressException(string message)
+         : base(message)
+        {
+        }
+
+        /// <summary>
+        /// This constructor allows to add a spezial message and an other exception.
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public InvalidAddressException(string message, System.Exception inner)
+            : base(message, inner)
+        {
+        }
+    }
+}
