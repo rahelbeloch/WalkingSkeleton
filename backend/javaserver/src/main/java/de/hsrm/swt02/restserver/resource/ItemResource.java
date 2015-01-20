1008dc3348637d6f988f92afa253de8f3e17530b
jvanh001 <jvanh001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-20 16:49:50 +0000

    #93 added saveitem method in resource file

--- backend/javaserver/src/main/java/de/hsrm/swt02/restserver/resource/ItemResource.java
+++ backend/javaserver/src/main/java/de/hsrm/swt02/restserver/resource/ItemResource.java
@@ -2,22 +2,28 @@ package de.hsrm.swt02.restserver.resource;
 
 import java.util.logging.Level;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.HeaderParam;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import de.hsrm.swt02.businesslogic.Logic;
+import de.hsrm.swt02.businesslogic.LogicResponse;
 import de.hsrm.swt02.businesslogic.exceptions.LogicException;
 import de.hsrm.swt02.constructionfactory.ConstructionFactory;
 import de.hsrm.swt02.logging.UseLogger;
 import de.hsrm.swt02.messaging.ServerPublisher;
 import de.hsrm.swt02.model.Item;
+import de.hsrm.swt02.model.Workflow;
 import de.hsrm.swt02.restserver.exceptions.JacksonException;
 
 /**
@@ -68,6 +74,51 @@ public class ItemResource {
         }
         LOGGER.log(Level.INFO, loggingBody + " Request successful.");
         return Response.ok(itemAsString).build();
+    }
+    
+
+    /**
+     * Receives an item and stores it into the database. This operation will
+     * be published on the message broker.
+     * @param formParams wrapper for an sent item
+     * @param username indicates which user wants to update the item
+     * @return "true" if everything was successful OR "jackson exception" if
+     *         serialization crashed
+     */
+    @POST
+    @Path("items")
+    @Produces(MediaType.TEXT_PLAIN)
+    @Consumes("application/x-www-form-urlencoded")
+    public Response saveItem(MultivaluedMap<String, String> formParams, @HeaderParam("username") String username) {
+        LogicResponse logicResponse;
+        final String loggingBody = PREFIX + "POST /resource/items";
+        LOGGER.log(Level.INFO, loggingBody);
+        final String itemAsString = formParams.get("data").get(0);
+        Item item = new Item();
+
+        try {
+            item = (Item)JsonParser.unmarshall(itemAsString, item);
+        } catch (JacksonException e) {
+            LOGGER.log(Level.INFO, loggingBody);
+            LOGGER.log(Level.WARNING, e);
+            return Response
+                    .serverError()
+                    .entity(String.valueOf(e
+                            .getErrorCode())).build();
+        }
+        try {
+            
+            logicResponse = LOGIC.updateItem(item, username);
+        } catch (LogicException e1) {
+            LOGGER.log(Level.WARNING, e1);
+            return Response.serverError()
+                    .entity(String.valueOf(e1.getErrorCode())).build();
+        }
+        
+        PUBLISHER.publishEvent(logicResponse);
+        
+        LOGGER.log(Level.INFO, loggingBody + " Item successfully saved.");
+        return Response.ok().build();
     }
     
 }
