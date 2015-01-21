1cc758552c7d95b857e896b501289b0dbc083e5b
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-21 09:12:49 +0000

    #53 removed temporary fix

--- frontend/Admin/ViewModel/LoginViewModel.cs
+++ frontend/Admin/ViewModel/LoginViewModel.cs
@@ -191,7 +191,7 @@ namespace Admin.ViewModel
             Match matchLocal = Regex.Match(matchString, @localPattern, RegexOptions.IgnoreCase);
 
             // check the match success
-            return true || matchIp.Success || matchLocal.Success;
+            return matchIp.Success || matchLocal.Success;
         }
     }
 }
