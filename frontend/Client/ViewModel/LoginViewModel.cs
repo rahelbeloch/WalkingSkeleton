85aa295ea95fdba2bdae0b3d6365f8338ec9eecb
rhaba001 <rhaba001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-21 10:27:03 +0000

    #111 released saving and validating server broker addresses to util class in ComLib; fixed the right validation

--- frontend/Client/ViewModel/LoginViewModel.cs
+++ frontend/Client/ViewModel/LoginViewModel.cs
@@ -118,13 +118,17 @@ namespace Client.ViewModel
                         try
                         {
                             SaveAddresses();
-                            _mainViewModel.myComLib.RefreshUrls(_serverAddress, _brokerAddress);
 
-                            // Register mainViewModel to CommunicationLib (if login worked)
-                            _mainViewModel.myComLib.Login(username, securePwd);
-                            logger.Info("Login successful for username=" + username + " password=" + securePwd);
-                            _mainViewModel.CurrentPageViewModel = _mainViewModel.dashboardViewModel;
-                            _mainViewModel.username = _username;
+                            if (_mainViewModel.myComLib != null)
+                            {
+                                _mainViewModel.myComLib.RefreshUrls(_serverAddress, _brokerAddress);
+
+                                // Register mainViewModel to CommunicationLib (if login worked)
+                                _mainViewModel.myComLib.Login(username, securePwd);
+                                logger.Info("Login successful for username=" + username + " password=" + securePwd);
+                                _mainViewModel.CurrentPageViewModel = _mainViewModel.dashboardViewModel;
+                                _mainViewModel.username = _username;
+                            }
                         }
                         catch (BasicException exc)
                         {
@@ -149,48 +153,11 @@ namespace Client.ViewModel
 
         private void SaveAddresses()
         {
-            string server = "http";
-            string broker = "tcp";
-
             // retrieve local config file path from filesystem
             string uriPath = new Uri(Assembly.GetExecutingAssembly().CodeBase).LocalPath;
-            // save server and broker address to config file
-            Configuration config = ConfigurationManager.OpenExeConfiguration(uriPath);
 
-            // test if given addresses are valid
-            if (ValidateAddress(server, _serverAddress) && ValidateAddress(broker, _brokerAddress))
-            {
-                if (config.AppSettings.Settings["ServerAddress"] == null && config.AppSettings.Settings["BrokerAddress"] == null)
-                {
-                    config.AppSettings.Settings.Add("ServerAddress", _serverAddress);
-                    config.AppSettings.Settings.Add("BrokerAddress", _brokerAddress);
-                }
-                else
-                {
-                    config.AppSettings.Settings["ServerAddress"].Value = _serverAddress;
-                    config.AppSettings.Settings["BrokerAddress"].Value = _brokerAddress;
-                }
-            }
-            else
-            {
-                throw new InvalidAddressException();
-            }
-
-            config.Save(ConfigurationSaveMode.Modified);
-            ConfigurationManager.RefreshSection("appSettings");
-        }
-
-        private bool ValidateAddress(string pre, string matchString)
-        {
-            string ipPattern = pre + "://\b(?:\\d{1,3}\\.){3}\\d{1,3}\\b:[0-9]{4,}";
-            string localPattern = pre + "://localhost:[0-9]{4,}";
-
-            // calling regex for ip/local server address
-            Match matchIp = Regex.Match(matchString, @ipPattern, RegexOptions.IgnoreCase);
-            Match matchLocal = Regex.Match(matchString, @localPattern, RegexOptions.IgnoreCase);
-
-            // check the match success
-            return matchIp.Success || matchLocal.Success;
+            // make use of util class in CommunicationLib
+            CommunicationLib.Util.SaveAddressesToConfig(uriPath, _brokerAddress, _serverAddress);
         }
     }
 }
