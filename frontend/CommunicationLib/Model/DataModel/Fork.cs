73e9ba5249b3cd9f8843354e38e5d4f923353469
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-23 09:58:11 +0000

    #113 added Fork model

---
+++ frontend/CommunicationLib/Model/DataModel/Fork.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommunicationLib.Model.DataModel
+{
+    class Fork : Step
+    {
+        /// <summary>
+        /// Used for (de)serialization. Do not change the property name.
+        /// </summary>
+        public string script { get { return _script; } set { _script = value; } }
+        private string _script;
+
+        /// <summary>
+        /// Constructor for Fork.
+        /// </summary>
+        public Fork()
+            : base()
+        {
+            script = "";
+        }
+    }
+}
