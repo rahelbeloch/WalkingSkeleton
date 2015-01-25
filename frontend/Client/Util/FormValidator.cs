989a5660a2516e0f84a9b35b5b32cfca492c6669
tginz001 <tginz001@027a9c09-b176-4c68-84bb-f1762c6bf0a4>
2015-01-25 13:17:37 +0000

    #146 improved regex for double values

--- frontend/Client/Util/FormValidator.cs
+++ frontend/Client/Util/FormValidator.cs
@@ -51,7 +51,7 @@ namespace Client.Util
                             regex = new Regex("[0-9]+");
                             break;
                         case "double":
-                            regex = new Regex("([0-9]|[1-9]+[0-9]*)(\\.?(?<!0)[0-9]{1,2}(?<!000))?$");
+                            regex = new Regex("^-?[0-9]*([.][0-9]*)?$");
                             break;
                         default:
                             return ValidationResult.ValidResult;
